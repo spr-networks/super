02bb9a2001e281a9108f14cc2f526b0c78e65526
root <alex+rad@longterm.io>
2021-10-07 00:11:45 +0000

    [wireguard] Fix wireguard startup script

--- wireguard/scripts/startup.sh
+++ wireguard/scripts/startup.sh
@@ -6,7 +6,7 @@ if [ "$WIREGUARD_NETWORK" ]; then
   ip link add dev wg0 type wireguard
   ip addr flush dev wg0
   ip addr add $WIREGUARD_NETWORK dev wg0
-  wg setconf wg0 /configs/wg0.conf
+  wg setconf wg0 /configs/wireguard/wg0.conf
   #ip route add $WIREGUARD_NETWORK dev wg0
   ip link set dev wg0 up
 fi
