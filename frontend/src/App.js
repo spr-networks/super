07bbeef47c72097aadd4326b784a84a5c6dbc451
lts-po <philip.olausson@longterm.io>
2022-03-28 14:44:39 +0200

    [ui] react test for App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,7 +22,7 @@ import AuthLayout from "layouts/Auth.js";
 import AdminLayout from "layouts/Admin.js";
 
 import "bootstrap/dist/css/bootstrap.css";
-import "assets/scss/paper-dashboard.scss?v=1.3.0";
+import "./assets/scss/paper-dashboard.scss";
 import "perfect-scrollbar/css/perfect-scrollbar.css";
 
 export default function App() {
