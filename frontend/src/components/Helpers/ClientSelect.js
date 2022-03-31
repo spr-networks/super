a7c9db9d045847405b20fe4e9c9c176b38186e23
lts-po <philip.olausson@longterm.io>
2022-03-31 16:01:42 +0200

    [ui] client select cleanup

--- frontend/src/components/Helpers/ClientSelect.js
+++ frontend/src/components/Helpers/ClientSelect.js
@@ -1,21 +1,23 @@
-import { useState } from "react"
-import Select from "react-select"
+port Select from "react-select"
 
 export default (props) => {
-  const [selected, setSelected] = useState(null)
   let devices = props.devices
-  let options = props.options
-  /*Object.values(devices)
-  .filter(d => d.RecentIP.length)
-  .map(d => { return {label: d.Name||d.RecentIP, value: d.RecentIP}})*/
-
+  let options = []
+  
+  if (props.devices) {
+    options = Object.values(devices)
+    .filter(d => d.RecentIP.length)
+    .map(d => { return {label: d.Name||d.RecentIP, value: d.RecentIP}})
+  } else {
+    options = props.options
+  }
+  
   let isMulti = props.isMulti || false
   let defaultValue = props.defaultValue || []
 
   return (
     <Select
-      isMulti={isMulti} 
-      isSearchable={true} 
+      isMulti={isMulti}
       options={options}
       value={defaultValue}
       onChange={props.onChange}
