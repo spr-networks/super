e442525436c3aa840e592494c31aa2b54fe90493
lts-po <philip.olausson@longterm.io>
2022-04-03 18:34:15 +0200

    [ui] initial test for client select

---
+++ frontend/src/__tests__/ClientSelect.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { cleanup, fireEvent, render } from '@testing-library/react'
+import ClientSelect from 'components/helpers/ClientSelect'
+
+afterEach(cleanup)
+
+it('renders single client select', () => {
+  const onChange = jest.fn()
+
+  const { baseElement, container } = render(
+    <ClientSelect onChange={onChange} />
+  )
+
+  expect(baseElement).toBeDefined()
+
+  //const input = container.querySelector('input')
+  expect(onChange).toHaveBeenCalledTimes(0)
+})
