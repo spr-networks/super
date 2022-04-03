a593d3559d25b0d2316017a9dfe07134dcb6d78c
lts-po <philip.olausson@longterm.io>
2022-04-03 13:31:32 +0200

    [ui] syntax

--- frontend/src/components/DNS/DNSAddBlocklist.js
+++ frontend/src/components/DNS/DNSAddBlocklist.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { APIErrorContext } from 'layouts/Admin'
-import {blockAPI} from 'api/DNS'
+import { blockAPI } from 'api/DNS'
 
 import {
   Button,
@@ -26,33 +26,36 @@ export default class DNSAddBlocklist extends React.Component {
   }
 
   handleChange(event) {
-    this.setState({URI: event.target.value})
+    this.setState({ URI: event.target.value })
   }
 
   handleSwitchChange(el, Enabled) {
-    this.setState({Enabled})
+    this.setState({ Enabled })
   }
 
   handleSubmit(event) {
     event.preventDefault()
 
-    let blocklist = {URI: this.state.URI, Enabled: this.state.Enabled}
-    blockAPI.putBlocklist(blocklist)
-      .then(res => {
+    let blocklist = { URI: this.state.URI, Enabled: this.state.Enabled }
+    blockAPI
+      .putBlocklist(blocklist)
+      .then((res) => {
         this.props.notifyChange('blocklists')
       })
-      .catch(error => {
-        this.context.reportError("API Failure: " + error.message)
+      .catch((error) => {
+        this.context.reportError('API Failure: ' + error.message)
       })
   }
 
   render() {
     return (
-			<Form onSubmit={this.handleSubmit}>
-				<Row>
-					<Label for="URI" sm={2}>URI</Label>
-					<Col sm={10}>
-						<FormGroup>
+      <Form onSubmit={this.handleSubmit}>
+        <Row>
+          <Label for="URI" sm={2}>
+            URI
+          </Label>
+          <Col sm={10}>
+            <FormGroup>
               <Input
                 type="text"
                 id="URI"
@@ -63,42 +66,53 @@ export default class DNSAddBlocklist extends React.Component {
                 autoFocus
               />
               <FormText tag="span">
-                <a target="_blank" href="https://github.com/StevenBlack/hosts">See here</a> for examples of host files to use
+                <a
+                  target="_blank"
+                  href="https://github.com/StevenBlack/hosts"
+                  rel="noreferrer"
+                >
+                  See here
+                </a>{' '}
+                for examples of host files to use
               </FormText>
-						</FormGroup>
-					</Col>
-				</Row>
+            </FormGroup>
+          </Col>
+        </Row>
 
-				<Row>
-					<Label for="Enabled" sm={2}>Enabled</Label>
-					<Col sm={10}>
-						<FormGroup check>
-							<Label check className="mb-2">
+        <Row>
+          <Label for="Enabled" sm={2}>
+            Enabled
+          </Label>
+          <Col sm={10}>
+            <FormGroup check>
+              <Label check className="mb-2">
                 <Input
                   type="checkbox"
                   checked={this.state.Enabled}
-                  onChange={(e) => this.handleSwitchChange(this, !this.state.Enabled)}
+                  onChange={(e) =>
+                    this.handleSwitchChange(this, !this.state.Enabled)
+                  }
                 />
-								<span className="form-check-sign" />
-							</Label>
-						</FormGroup>
-					</Col>
-				</Row>
+                <span className="form-check-sign" />
+              </Label>
+            </FormGroup>
+          </Col>
+        </Row>
 
-				<Row>
-					<Col sm={{offset: 2, size: 10}}>
-						<Button
-							className="btn-round"
-							color="primary"
-							size="md"
-							type="submit"
-							onClick={this.handleSubmit}
-						>
-						Save
-						</Button>
-					</Col>
-				</Row>
-			</Form>
+        <Row>
+          <Col sm={{ offset: 2, size: 10 }}>
+            <Button
+              className="btn-round"
+              color="primary"
+              size="md"
+              type="submit"
+              onClick={this.handleSubmit}
+            >
+              Save
+            </Button>
+          </Col>
+        </Row>
+      </Form>
     )
   }
 }
