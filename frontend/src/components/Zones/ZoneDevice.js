7a7c03eb7fce9bae7528bc23340c3321d3d8075e
lts-po <philip.olausson@longterm.io>
2022-04-03 10:41:26 +0200

    [ui] cleanup

--- frontend/src/components/Zones/ZoneDevice.js
+++ frontend/src/components/Zones/ZoneDevice.js
@@ -1,20 +1,4 @@
-import { Component } from "react";
-import { zoneDescriptions } from "components/Helpers/Api.js";
-import {
-  Button,
-  ButtonGroup,
-  Card,
-  CardHeader,
-  CardBody,
-  CardTitle,
-  Label,
-  Table,
-  Row,
-  Col,
-  UncontrolledTooltip,
-} from "reactstrap";
-import Select from "react-select";
-
+import { Component } from 'react'
 
 export default class ZoneDevice extends Component {
 
@@ -28,12 +12,7 @@ export default class ZoneDevice extends Component {
     const setState = (v) => {
       this.setState(v)
     }
-
-    const device = this.props.device;
-    //const generatedID = Math.random().toString(36).substr(2, 9);
-
   }
-
 
   render() {
     const device = this.props.device;
