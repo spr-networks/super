c16077cff4e19ebb04d0981f0ce59c27c8e6021b
lts-po <philip.olausson@longterm.io>
2022-04-03 13:57:06 +0200

    [ui] cleanup + define proptypes

--- frontend/src/components/DNS/DNSLogList.js
+++ frontend/src/components/DNS/DNSLogList.js
@@ -1,9 +1,10 @@
 import React, { useContext, useRef } from 'react'
-import DNSAddLog from "components/DNS/DNSAddLog"
-import ModalForm from "components/ModalForm"
+import PropTypes from 'prop-types'
+import DNSAddLog from 'components/DNS/DNSAddLog'
+import ModalForm from 'components/ModalForm'
 import { APIErrorContext } from 'layouts/Admin'
-import ReactBSAlert from "react-bootstrap-sweetalert"
-import { logAPI } from "api/DNS"
+import ReactBSAlert from 'react-bootstrap-sweetalert'
+import { logAPI } from 'api/DNS'
 
 import {
   Button,
@@ -11,27 +12,24 @@ import {
   CardHeader,
   CardBody,
   CardTitle,
-  Table,
-  Row,
-  Col,
-  Modal,
-} from "reactstrap"
+  Table
+} from 'reactstrap'
 
 export default class DNSLogList extends React.Component {
-  static contextType = APIErrorContext;
+  static contextType = APIErrorContext
   state = { type: '', list: [], showAlert: false }
 
   constructor(props) {
     super(props)
 
-    this.deleteListItem = this.deleteListItem.bind(this);
-    this.addListItem = this.addListItem.bind(this);
-    this.triggerAlert = this.triggerAlert.bind(this);
+    this.deleteListItem = this.deleteListItem.bind(this)
+    this.addListItem = this.addListItem.bind(this)
+    this.triggerAlert = this.triggerAlert.bind(this)
 
     this.state.list = []
     this.state.type = props.type
 
-		this.refModal = React.createRef()
+    this.refModal = React.createRef()
   }
 
   async componentDidMount() {
@@ -48,24 +46,24 @@ export default class DNSLogList extends React.Component {
         list = await logAPI.hostPrivacyList()
       }
 
-      this.setState({list})
-    } catch(error) {
-      this.context.reportError("API Failure: " + error.message)
+      this.setState({ list })
+    } catch (error) {
+      this.context.reportError('API Failure: ' + error.message)
     }
   }
 
   triggerAlert(show) {
     if (show === false) {
-      return  this.setState({showAlert: false})
+      return this.setState({ showAlert: false })
     }
 
-    this.setState({showAlert: true})
+    this.setState({ showAlert: true })
   }
 
   addListItem(item) {
     let list = this.state.list
     list.push(item)
-    this.setState({list})
+    this.setState({ list })
     if (this.state.type == 'Domain') {
       logAPI.addDomainIgnores(item)
     } else {
@@ -76,15 +74,16 @@ export default class DNSLogList extends React.Component {
   deleteListItem(item) {
     if (this.state.type == 'Domain') {
       return alert(`TODO: delete ${item}`)
-    } 
+    }
 
-    let list = this.state.list.filter(_item => _item != item)
-    logAPI.putHostPrivacyList(list)
-      .then(res => {
-        this.setState({list})
+    let list = this.state.list.filter((_item) => _item != item)
+    logAPI
+      .putHostPrivacyList(list)
+      .then((res) => {
+        this.setState({ list })
       })
-      .catch(error => {
-        this.context.reportError("API Failure: " + error.message)
+      .catch((error) => {
+        this.context.reportError('API Failure: ' + error.message)
       })
   }
 
@@ -100,10 +99,14 @@ export default class DNSLogList extends React.Component {
 
     return (
       <>
-
         <Card>
           <CardHeader>
-            <Button className="btn-round pull-right" color="primary" outline onClick={this.triggerAlert}>
+            <Button
+              className="btn-round pull-right"
+              color="primary"
+              outline
+              onClick={this.triggerAlert}
+            >
               <i className="fa fa-plus" /> add
             </Button>
 
@@ -118,37 +121,36 @@ export default class DNSLogList extends React.Component {
               cancelBtnBsStyle="danger"
               openAnim={false}
               btnSize=""
-              />
+            />
 
             <CardTitle tag="h4">{this.props.title}</CardTitle>
             <p className="text-muted">{this.props.description}</p>
           </CardHeader>
           <CardBody>
-            <Table responsive className={!list.length ? "d-none" : null}>
+            <Table responsive className={!list.length ? 'd-none' : null}>
               <thead className="text-primary">
                 <tr>
-                  <th style={{width: "100%"}}>{type}</th>
+                  <th style={{ width: '100%' }}>{type}</th>
                   <th className="text-center">Actions</th>
                 </tr>
               </thead>
               <tbody>
-                {
-                  list.map(item => (
-                    <tr key={item}>
-                      <td>{item}</td>
-                      <td className="text-center">
-                        <Button
-                          className="btn-icon"
-                          color="danger"
-                          size="sm"
-                          type="button"
-                          onClick={(e) => this.deleteListItem(item)}>
-                          <i className="fa fa-times" />
-                        </Button>
+                {list.map((item) => (
+                  <tr key={item}>
+                    <td>{item}</td>
+                    <td className="text-center">
+                      <Button
+                        className="btn-icon"
+                        color="danger"
+                        size="sm"
+                        type="button"
+                        onClick={(e) => this.deleteListItem(item)}
+                      >
+                        <i className="fa fa-times" />
+                      </Button>
                     </td>
-                    </tr>
-                  ))
-                }
+                  </tr>
+                ))}
               </tbody>
             </Table>
           </CardBody>
@@ -156,4 +158,10 @@ export default class DNSLogList extends React.Component {
       </>
     )
   }
+}
+
+DNSLogList.propTypes = {
+  type: PropTypes.string,
+  title: PropTypes.string,
+  description: PropTypes.string
 }
