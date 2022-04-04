13039fbc37251eec6ccda45a9209af39068307bb
lts-po <philip.olausson@longterm.io>
2022-04-04 16:08:31 +0200

    [ui] updated chart component for signal strength + rx/tx rate

--- frontend/src/views/SignalStrength.js
+++ frontend/src/views/SignalStrength.js
@@ -4,12 +4,13 @@ import { Chart as ChartJS } from 'chart.js/auto'
 import { Bar } from 'react-chartjs-2'
 import { hostapdAllStations, getArp, getDevices } from 'components/Helpers/Api'
 import { APIErrorContext } from 'layouts/Admin'
-import {
+
+/*import {
   UncontrolledDropdown,
   DropdownToggle,
   DropdownMenu,
   DropdownItem
-} from 'reactstrap'
+} from 'reactstrap'*/
 
 import {
   Card,
@@ -22,17 +23,44 @@ import {
 } from 'reactstrap'
 
 export default class SignalStrength extends Component {
-  state = { signals: {}, signal_scale: 'All Time' }
+  state = {
+    signals: {},
+    signals_rssi: {},
+    signals_rxtx: {},
+    signal_scale: 'All Time'
+  }
 
   static contextType = APIErrorContext
   macToName = {}
   macToIP = {}
 
-  async processTrafficHistory(target, scale) {
-    const devices = await getDevices().catch((error) => {
+  clientLabelFromMAC(mac) {
+    let name = this.macToName[mac]
+    let ip = this.macToIP[mac]
+    return `${name} ${ip}`
+  }
+
+  async fetchData() {
+    const stations = await hostapdAllStations().catch((error) => {
       this.context.reportError('API Failure get traffic: ' + error.message)
     })
 
+    let signals = []
+    for (let mac in stations) {
+      let station = stations[mac]
+      signals.push({
+        MAC: mac,
+        Signal: parseInt(station.signal),
+        TX: parseInt(station.tx_rate_info),
+        RX: parseInt(station.rx_rate_info)
+      })
+    }
+
+    return signals
+  }
+
+  async componentDidMount() {
+    // this is for mac => ip lookup in tooltip
     const arp = await getArp().catch((error) => {
       this.context.reportError('API Failure get traffic: ' + error.message)
     })
@@ -42,191 +70,102 @@ export default class SignalStrength extends Component {
       if (a.MAC == '00:00:00:00:00:00') {
         continue
       }
+
       this.macToIP[a.MAC] = a.IP
     }
+
+    // get devices for mac => name lookup
+    const devices = await getDevices().catch((error) => {
+      this.context.reportError('API Failure get traffic: ' + error.message)
+    })
 
     Object.keys(devices).forEach((mac) => {
       this.macToName[mac] = devices[mac].Name
     })
 
-    let processData = (signals) => {
-      let data = {
-        labels: [],
-        datasets: [
-          {
-            label: 'Signal',
-            yAxisID: 'RSSI',
-            fill: true,
-            backgroundColor: [],
-            borderWidth: 1,
-            barPercentage: 0.5,
-            data: []
-          } /*
-            {
-              label: "RX Rate",
-              yAxisID: 'Rate',
-              fill: true,
-              backgroundColor: "#4cbdd7",
-              borderWidth: 1,
-              barPercentage: 0.5,
-              data: [
-              ],
-            },
-            {
-              label: "TX Rate",
-              yAxisID: 'Rate',
-              fill: true,
-              backgroundColor: "#4cbdd7",
-              borderWidth: 1,
-              barPercentage: 0.5,
-              data: [
-              ],
-            }*/
-        ]
+    const signals = await this.fetchData()
+    this.setState({ signals })
+
+    let signals_rssi = this.processData('RSSI')
+    let signals_rxtx = this.processData(['RX', 'TX'])
+
+    this.setState({ signals_rssi })
+    this.setState({ signals_rxtx })
+  }
+
+  processData(labels = ['RSSI']) {
+    labels = Array.isArray(labels) ? labels : [labels]
+
+    const signalToColor = (signal) => {
+      if (signal >= -60) {
+        return 'rgb(24, 206, 15)'
+      } else if (signal >= -70) {
+        return 'rgb(44, 168, 255)'
+      } else if (signal >= -80) {
+        return 'rgb(255, 178, 54)'
       }
 
-      let d_labels = []
-      let d_signal = []
-      let d_tx = []
-      let d_rx = []
-      let d_colors = []
-      for (const entry of signals) {
-        d_labels.push(entry.MAC)
-        d_signal.push(entry.Signal)
-        d_tx.push(entry.TX)
-        d_rx.push(entry.RX)
-        let color = ''
-        //match _variables.scss
-        if (entry.Signal >= -60) {
-          color = 'rgb(24, 206, 15)'
-        } else if (entry.Signal >= -70) {
-          color = 'rgb(44, 168, 255)'
-        } else if (entry.Signal >= -80) {
-          color = 'rgb(255, 178, 54)'
-        } else {
-          color = 'rgb(255, 54, 54)'
-        }
-        d_colors.push(color)
-      }
-
-      data.labels = d_labels
-      data.datasets[0].data = d_signal
-      data.datasets[0].backgroundColor = d_colors
-      /*
-        data.datasets[1].data = d_rx
-        data.datasets[2].data = d_tx
-        */
-      return data
+      return 'rgb(255, 54, 54)'
     }
 
-    if (scale == 'All Time') {
-      //data for all time traffic
-      const stations = await hostapdAllStations().catch((error) => {
-        this.context.reportError('API Failure get traffic: ' + error.message)
+    let data = {
+      labels: [],
+      datasets: []
+    }
+
+    let labelColor = {
+      RX: '#4cbd4c',
+      TX: '#4cbdd7',
+      RSSI: []
+    }
+
+    labels.map((label) => {
+      let backgroundColor = labelColor[label]
+      let dataset = {
+        label,
+        fill: true,
+        backgroundColor,
+        borderWidth: 1,
+        barPercentage: 0.5,
+        data: []
+      }
+
+      // if multiple datasets - use the same yAxis
+      if (labels.length > 1) {
+        dataset.yAxisID = labels.join('')
+      }
+
+      data.datasets.push(dataset)
+    })
+
+    let signals = this.state.signals
+    for (const entry of signals) {
+      data.labels.push(entry.MAC)
+
+      labels.map((label, index) => {
+        if (label == 'RSSI') {
+          data.datasets[index].data.push(entry.Signal)
+
+          let color = signalToColor(entry.Signal)
+          data.datasets[index].backgroundColor.push(color)
+        }
+
+        if (label == 'RX') {
+          data.datasets[index].data.push(entry.RX)
+        }
+
+        if (label == 'TX') {
+          data.datasets[index].data.push(entry.TX)
+        }
       })
-      let signals = []
-      for (let mac in stations) {
-        let station = stations[mac]
-        signals.push({
-          MAC: mac,
-          Signal: parseInt(station.signal),
-          TX: parseInt(station.tx_rate_info),
-          RX: parseInt(station.rx_rate_info)
-        })
-      }
-      return processData(signals)
     }
-  }
+    console.log(data)
 
-  async componentDidMount() {
-    let signal_data = await this.processTrafficHistory(
-      'signal',
-      this.state.signal_scale
-    )
-    this.setState({ signals: signal_data })
-  }
-
-  templateData = {
-    options: {
-      indexAxis: 'x',
-      plugins: {
-        legend: {
-          display: false
-        },
-
-        tooltip: {
-          callbacks: {
-            beforeBody: (TooltipItems, object) => {
-              let mac = TooltipItems[0].label
-              let ip = this.macToIP[mac]
-              let name = this.macToName[mac]
-              let label = ''
-              if (name) {
-                label = label + '  ' + name
-              }
-              if (ip) {
-                label = label + '  ' + ip
-              }
-
-              return label
-            }
-          }
-        },
-
-        tooltips: {
-          tooltipFillColor: 'rgba(0,0,0,0.5)',
-          tooltipFontFamily:
-            "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
-          tooltipFontSize: 14,
-          tooltipFontStyle: 'normal',
-          tooltipFontColor: '#fff',
-          tooltipTitleFontFamily:
-            "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
-          tooltipTitleFontSize: 14,
-          tooltipTitleFontStyle: 'bold',
-          tooltipTitleFontColor: '#fff',
-          tooltipYPadding: 6,
-          tooltipXPadding: 6,
-          tooltipCaretSize: 8,
-          tooltipCornerRadius: 6,
-          tooltipXOffset: 10
-        }
-      },
-      scales: {
-        RSSI: {
-          position: 'left',
-          type: 'linear',
-          ticks: {
-            includeBounds: true,
-            color: '#9f9f9f'
-          },
-          grid: {
-            zeroLineColor: 'transparent',
-            display: true,
-            drawBorder: false,
-            color: '#9f9f9f'
-          }
-        },
-        Rate: {
-          position: 'right',
-          type: 'linear',
-          ticks: {
-            includeBounds: true,
-            color: '#9f9f9f'
-          },
-          grid: {
-            zeroLineColor: 'transparent',
-            display: true,
-            drawBorder: false,
-            color: '#9f9f9f'
-          }
-        }
-      }
-    }
+    return data
   }
 
   render() {
-    let handleScaleMenu = (e) => {
+    /*let handleScaleMenu = (e) => {
       let choice = e.target.parentNode.getAttribute('value')
       let scale = e.target.value
 
@@ -236,13 +175,59 @@ export default class SignalStrength extends Component {
         o[choice] = result
         this.setState(o)
       })
+    }*/
+
+    let options = {
+      indexAxis: 'x',
+      plugins: {
+        legend: { display: false },
+
+        tooltip: {
+          callbacks: {
+            beforeBody: (tooltipItems) =>
+              this.clientLabelFromMAC(tooltipItems[0].label)
+          }
+        }
+      },
+      scales: {}
     }
+
+    let options_rssi = Object.assign(options, {
+      scales: {
+        RSSI: {
+          position: 'right',
+          type: 'linear',
+          ticks: {
+            includeBounds: true,
+            color: '#9f9f9f'
+          } /*
+          grid: {
+            zeroLineColor: 'transparent',
+            display: true,
+            drawBorder: false,
+            color: '#f0f0f0'
+          }*/
+        }
+      }
+    })
+
+    let options_rxtx = Object.assign(options, {
+      scales: {
+        RXTX: {
+          position: 'right',
+          type: 'linear',
+          ticks: {
+            includeBounds: true,
+            color: '#9f9f9f'
+          }
+        }
+      }
+    })
 
     return (
       <div className="content">
-        <Row>
-          <Col md="10"></Col>
-          <Col md="2">
+        {/*<Row>
+          <Col md={{ size: 2, offset: 10 }}>
             <UncontrolledDropdown group>
               <DropdownToggle caret color="default">
                 {this.state.signal_scale}
@@ -254,7 +239,7 @@ export default class SignalStrength extends Component {
               </DropdownMenu>
             </UncontrolledDropdown>
           </Col>
-        </Row>
+        </Row>*/}
         <Row>
           <Col md="12">
             <Card>
@@ -262,11 +247,22 @@ export default class SignalStrength extends Component {
                 <CardTitle tag="h4">Device Signal Strength (RSSI)</CardTitle>
               </CardHeader>
               <CardBody>
-                {this.state.signals.datasets ? (
-                  <Bar
-                    data={this.state.signals}
-                    options={this.templateData.options}
-                  />
+                {this.state.signals_rssi.datasets ? (
+                  <Bar data={this.state.signals_rssi} options={options_rssi} />
+                ) : null}
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col md="12">
+            <Card>
+              <CardHeader>
+                <CardTitle tag="h4">Device RX/TX Rate</CardTitle>
+              </CardHeader>
+              <CardBody>
+                {this.state.signals_rxtx.datasets ? (
+                  <Bar data={this.state.signals_rxtx} options={options_rxtx} />
                 ) : null}
               </CardBody>
             </Card>
