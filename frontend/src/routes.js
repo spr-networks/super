9b5093219c1a6b0492788dfe5747944547a48489
lts-po <philip.olausson@longterm.io>
2022-04-04 22:41:36 +0200

    [ui] cleanup + add dnslog route for ip + filtertext in url

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,157 +1,156 @@
-import AddDevice from "views/Devices/AddDevice.js";
-import Arp from "views/Devices/Arp.js";
-import Devices from "views/Devices/Devices.js";
-import Dhcp from "views/Zones/Dhcp.js";
-import Home from "views/Home.js";
-import Login from "views/pages/Login.js";
-import SignalStrength from "views/SignalStrength.js";
-import Traffic from "views/Traffic.js";
-import TrafficTimeSeries from "views/TrafficTimeSeries.js";
-import WirelessConfiguration from "views/WirelessConfiguration"
-import Zones from "views/Zones/Zones"
-import DNSBlock from "views/DNS/DNSBlock"
-import DNSLog from "views/DNS/DNSLog"
-import DNSLogEdit from "views/DNS/DNSLogEdit"
+import AddDevice from 'views/Devices/AddDevice.js'
+import Arp from 'views/Devices/Arp.js'
+import Devices from 'views/Devices/Devices.js'
+import Dhcp from 'views/Zones/Dhcp.js'
+import Home from 'views/Home.js'
+import Login from 'views/pages/Login.js'
+import SignalStrength from 'views/SignalStrength.js'
+import Traffic from 'views/Traffic.js'
+import TrafficTimeSeries from 'views/TrafficTimeSeries.js'
+import WirelessConfiguration from 'views/WirelessConfiguration'
+import Zones from 'views/Zones/Zones'
+import DNSBlock from 'views/DNS/DNSBlock'
+import DNSLog from 'views/DNS/DNSLog'
+import DNSLogEdit from 'views/DNS/DNSLogEdit'
 
 const routes = [
   {
-    path: "/home",
-    name: "Home",
-    icon: "nc-icon nc-planet",
+    path: '/home',
+    name: 'Home',
+    icon: 'nc-icon nc-planet',
     component: Home,
-    layout: "/admin",
+    layout: '/admin'
   },
   {
     collapse: true,
-    name: "Devices",
-    icon: "nc-icon nc-laptop",
-    state: "devicesCollapse",
+    name: 'Devices',
+    icon: 'nc-icon nc-laptop',
+    state: 'devicesCollapse',
     views: [
       {
-        path: "/devices",
-        name: "List",
-        icon: "fa fa-laptop",
+        path: '/devices',
+        name: 'List',
+        icon: 'fa fa-laptop',
         component: Devices,
-        layout: "/admin",
+        layout: '/admin'
       },
       {
-        path: "/add_device",
-        name: "Add WiFi Device",
-        icon: "fa fa-plus-square",
+        path: '/add_device',
+        name: 'Add WiFi Device',
+        icon: 'fa fa-plus-square',
         component: AddDevice,
-        layout: "/admin",
+        layout: '/admin'
       }
     ]
   },
   {
     collapse: true,
-    name: "Wireless Settings",
-    icon: "nc-icon nc-settings",
-    state: "wirelessCollapse",
+    name: 'Wireless Settings',
+    icon: 'nc-icon nc-settings',
+    state: 'wirelessCollapse',
     views: [
       {
-        path: "/wireless",
-        name: "Wireless",
-        icon: "fa fa-wifi",
+        path: '/wireless',
+        name: 'Wireless',
+        icon: 'fa fa-wifi',
         component: WirelessConfiguration,
-        layout: "/admin",
+        layout: '/admin'
       }
     ]
   },
   {
     collapse: true,
-    name: "System",
-    icon: "nc-icon nc-bullet-list-67",
-    state: "systemCollapse",
+    name: 'System',
+    icon: 'nc-icon nc-bullet-list-67',
+    state: 'systemCollapse',
     views: [
       {
-        path: "/dhcp",
-        name: "DHCP Table",
-        mini: "DHCP",
+        path: '/dhcp',
+        name: 'DHCP Table',
+        mini: 'DHCP',
         component: Dhcp,
-        layout: "/admin",
+        layout: '/admin'
       },
       {
-        path: "/arp",
-        name: "ARP Table",
-        mini: "ARP",
+        path: '/arp',
+        name: 'ARP Table',
+        mini: 'ARP',
         component: Arp,
-        layout: "/admin",
+        layout: '/admin'
       },
       {
-        path: "/zones",
-        name: "Show Zones",
-        icon: "fa fa-tags",
+        path: '/zones',
+        name: 'Show Zones',
+        icon: 'fa fa-tags',
         component: Zones,
-        layout: "/admin",
+        layout: '/admin'
       }
     ]
   },
   {
     collapse: true,
-    name: "Traffic",
-    icon: "nc-icon nc-chart-bar-32",
-    state: "trafficCollapse",
+    name: 'Traffic',
+    icon: 'nc-icon nc-chart-bar-32',
+    state: 'trafficCollapse',
     views: [
       {
-        path: "/traffic",
-        name: "Bandwidth Summary",
-        mini: "SU",
+        path: '/traffic',
+        name: 'Bandwidth Summary',
+        mini: 'SU',
         component: Traffic,
-        layout: "/admin",
+        layout: '/admin'
       },
       {
-        path: "/timeseries",
-        name: "Bandwidth Timeseries",
-        mini: "TS",
+        path: '/timeseries',
+        name: 'Bandwidth Timeseries',
+        mini: 'TS',
         component: TrafficTimeSeries,
-        layout: "/admin",
+        layout: '/admin'
       },
       {
-        path: "/signal/strength",
-        name: "Signal Strength",
-        mini: "SS",
+        path: '/signal/strength',
+        name: 'Signal Strength',
+        mini: 'SS',
         component: SignalStrength,
-        layout: "/admin",
+        layout: '/admin'
       }
     ]
   },
   {
     collapse: true,
-    name: "DNS",
-    icon: "nc-icon nc-world-2",
-    state: "dnsCollapse",
+    name: 'DNS',
+    icon: 'nc-icon nc-world-2',
+    state: 'dnsCollapse',
     views: [
       {
-        path: "/dnsBlock",
-        name: "Blocklists/Ad-Block",
-        icon: "fa fa-ban",
+        path: '/dnsBlock',
+        name: 'Blocklists/Ad-Block',
+        icon: 'fa fa-ban',
         component: DNSBlock,
-        layout: "/admin",
+        layout: '/admin'
       },
       {
-        path: "/dnsLog/:ips",
-        name: "DNS Log",
-        icon: "fa fa-th-list",
+        path: '/dnsLog/:ips/:text?',
+        name: 'DNS Log',
+        icon: 'fa fa-th-list',
         component: DNSLog,
-        layout: "/admin",
+        layout: '/admin'
       },
       {
-        path: "/dnsLogEdit",
-        name: "DNS Log Settings",
-        icon: "fa fa-cogs",
+        path: '/dnsLogEdit',
+        name: 'DNS Log Settings',
+        icon: 'fa fa-cogs',
         component: DNSLogEdit,
-        layout: "/admin",
-      },
+        layout: '/admin'
+      }
     ]
   },
 
   {
-    path: "/login",
+    path: '/login',
     component: Login,
-    layout: "/auth",
-  },
+    layout: '/auth'
+  }
+]
 
-];
-
-export default routes;
+export default routes
