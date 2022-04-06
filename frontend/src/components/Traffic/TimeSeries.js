16c15ea4fed9996bf389adff79dee0959e4541d5
lts-po <philip.olausson@longterm.io>
2022-04-06 13:03:10 +0200

    [ui] add traffic components

---
+++ frontend/src/components/Traffic/TimeSeries.js
@@ -0,0 +1,132 @@
+import React, { useState, useRef } from 'react'
+import PropTypes from 'prop-types'
+import Select from 'react-select'
+
+import ClientSelect from 'components/Helpers/ClientSelect'
+import TimeSeriesChart from 'components/Traffic/TimeSeriesChart'
+import TimeSeriesList from 'components/Traffic/TimeSeriesList'
+
+// this one show either a chart or table
+
+import {
+  Button,
+  ButtonGroup,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardTitle,
+  Row,
+  Col
+} from 'reactstrap'
+
+const TimeSeries = (props) => {
+  const scales = [
+    { value: 'All Time', label: 'All Time' },
+    { value: '1 Day', label: 'Last day' },
+    { value: '1 Hour', label: 'Last hour' },
+    { value: '15 Minutes', label: 'Last 15 minutes' }
+  ]
+  const [filterIPs, setFilterIPs] = useState([])
+  const [view, setView] = useState('chart')
+  const [scale, setScale] = useState('All Time')
+  const [offset, setOffset] = useState(0)
+
+  const handleTimeChange = (newValue) => {
+    setScale(newValue.label)
+    setOffset(newValue.value)
+    if (props.handleTimeChange) {
+      props.handleTimeChange(newValue.value, props.type)
+    }
+  }
+
+  const handleClientChange = (selectedIPs) => {
+    //this.setState({ selectedIPs })
+    let ips = selectedIPs.map((item) => item.value)
+    setFilterIPs(ips)
+  }
+
+  const handleClientClick = (ip, datapoint) => {
+    /*
+    const { x: ts } = datapoint
+    let d = new Date(ts)
+    let filterText = d.toISOString()
+    filterText += '-' + new Date(d.getTime() + 60 * 1e3).toISOString()
+    //this.props.history.push(`/admin/dnsLog/${ip}/${filterText}`)
+    */
+    console.log('updating client ip:', ip)
+    setFilterIPs([ip])
+    setView('table')
+    //props.handleClientClick(ip, datapoint)
+  }
+
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <Row>
+            <Col md="4">
+              <CardTitle tag="h4">{props.title || props.type}</CardTitle>
+            </Col>
+            <Col md="4" className="pt-2">
+              <div className={view == 'chart' ? 'd-none' : null}>
+                <ClientSelect
+                  isMulti
+                  value={filterIPs}
+                  onChange={handleClientChange}
+                />
+              </div>
+            </Col>
+            <Col md="2" className="pt-2">
+              <Select
+                onChange={handleTimeChange}
+                options={scales}
+                value={{ value: offset, label: scale }}
+              />
+            </Col>
+            <Col md="2" className="text-right">
+              <ButtonGroup>
+                <Button
+                  size="sm"
+                  color="primary"
+                  outline={view == 'chart' ? null : 'outline'}
+                  onClick={(e) => setView('chart')}
+                >
+                  <i className="fa fa-bar-chart" />
+                </Button>
+                <Button
+                  size="sm"
+                  color="primary"
+                  outline={view == 'table' ? null : 'outline'}
+                  onClick={(e) => setView('table')}
+                >
+                  <i className="fa fa-table" />
+                </Button>
+              </ButtonGroup>
+            </Col>
+          </Row>
+        </CardHeader>
+        <CardBody>
+          {view == 'table' ? (
+            <>
+              <TimeSeriesList
+                type={props.type}
+                offset={offset}
+                ips={filterIPs}
+              />
+            </>
+          ) : (
+            <TimeSeriesChart
+              type={props.type}
+              title={props.title}
+              data={props.data}
+              handleClientClick={handleClientClick}
+            />
+          )}
+        </CardBody>
+      </Card>
+    </>
+  )
+}
+
+export default TimeSeries
---
+++ frontend/src/components/Traffic/TimeSeriesList.js
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
+
+import { trafficAPI } from 'api/Traffic'
+import { prettyDate, prettySize } from 'utils'
+
+import { Table, Row, Col } from 'reactstrap'
+
+const TimeSeriesList = (props) => {
+  const [list, setList] = useState([])
+
+  // filter the list depending on the interface to match the type
+  const filterType = (_list, type) => {
+    return _list.filter((row) => {
+      if (type == 'WanIn' && row.Interface == 'wlan0') {
+        return row
+      }
+
+      if (type == 'WanOut' && row.Interface != 'wlan0') {
+        return row
+      }
+    })
+  }
+
+  useEffect(() => {
+    trafficAPI.traffic().then((data) => {
+      data = filterType(data, props.type)
+      // the data we fetch is from now and sorted desc - 1 minute for each row
+      let date = new Date()
+      date.setSeconds(0)
+      data = data.map((row) => {
+        date.setMinutes(date.getMinutes() - 1)
+        row.Timestamp = new Date(date)
+        return row
+      })
+
+      setList(data)
+    })
+  }, [])
+
+  let listFiltered = list
+
+  // filter by type
+  if (props.type) {
+  }
+
+  // filter by ip
+  if (props.ips && props.ips.length) {
+    let ips = props.ips
+    listFiltered = listFiltered.filter((row) => ips.includes(row.Src))
+  }
+
+  // filter by date
+  if (props.offset) {
+    const scaleOffset = {
+      '1 Hour': 60 - 1,
+      '1 Day': 60 * 24 - 1,
+      '15 Minutes': 15 - 1
+    }
+
+    let offset = scaleOffset[props.offset] || 0
+    let d = new Date()
+    d.setMinutes(d.getMinutes() - offset)
+    listFiltered = offset
+      ? listFiltered.filter((row) => row.Timestamp > d)
+      : listFiltered
+  }
+
+  return (
+    <Table responsive>
+      <thead className="text-primary">
+        <tr>
+          <th>Timestamp</th>
+          <th>Interface</th>
+          <th>Src IP</th>
+          <th>Dst IP</th>
+          <th>Size</th>
+        </tr>
+      </thead>
+      <tbody>
+        {listFiltered.map((row) => (
+          <tr>
+            <td>{prettyDate(row.Timestamp)}</td>
+            <td>{row.Interface}</td>
+            <td>{row.Src}</td>
+            <td>{row.Dst}</td>
+            {/*<td>{row.Packets}</td>*/}
+            <td>{prettySize(row.Bytes)}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  )
+}
+
+export default TimeSeriesList
