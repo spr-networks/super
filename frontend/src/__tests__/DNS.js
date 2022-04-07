9295d6f1338f729d5ff1fb4c0555e9e867dcafe6
lts-po <philip.olausson@longterm.io>
2022-04-07 15:32:23 +0200

    [ui] test blocklist list and remove

--- frontend/src/__tests__/DNS.js
+++ frontend/src/__tests__/DNS.js
@@ -1,5 +1,13 @@
 import React from 'react'
-import { render, screen, waitFor } from '@testing-library/react'
+import {
+  render,
+  screen,
+  fireEvent,
+  waitFor,
+  waitForElementToBeRemoved
+} from '@testing-library/react'
+import { within } from '@testing-library/dom'
+
 import DNSBlock from 'views/DNS/DNSBlock'
 //import DNSBlocklist from 'components/DNS/DNSBlocklist'
 //import DNSOverrideList from 'components/DNS/DNSOverrideList'
@@ -8,17 +16,45 @@ import { saveLogin, blockAPI } from 'api'
 beforeAll(() => saveLogin('admin', 'admin'))
 
 describe('DNS Block', () => {
+  let container = null
+
+  beforeEach(() => {
+    container = render(<DNSBlock />)
+  })
+
   test('DNS block list', async () => {
-    const { baseElement } = render(<DNSBlock />)
-    expect(baseElement).toBeDefined()
+    // make sure we have all the tables in the document
     expect(screen.getByText('DNS Blocklists')).toBeInTheDocument()
     expect(screen.getByText('Blocked Domain Override')).toBeInTheDocument()
     expect(screen.getByText('Allow Domain Override')).toBeInTheDocument()
 
+    // wait for data to be populated
     await waitFor(() =>
       expect(screen.getByText('example.com.')).toBeInTheDocument()
     )
+
+    // find override content
     expect(screen.getByText('192.168.2.102')).toBeInTheDocument()
+  })
+
+  test('remove blocklist item', async () => {
+    // wait fo async data
+    await waitFor(() =>
+      expect(screen.getByText('example.com.')).toBeInTheDocument()
+    )
+
+    let tables = screen.getAllByRole('table'),
+      blocklist = within(tables[0]), // blocklist is the first table
+      rows = blocklist.getAllByRole('row'),
+      lastRow = rows[rows.length - 1]
+
+    let buttonRemove = within(lastRow).getByRole('button')
+
+    expect(rows.length).toBeGreaterThanOrEqual(2)
+
+    fireEvent.click(buttonRemove)
+
+    await waitForElementToBeRemoved(lastRow)
   })
 })
 
