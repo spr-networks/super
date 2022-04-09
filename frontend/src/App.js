0829cdc3a200df7afe1866cf798977d213381a67
lts-po <philip.olausson@longterm.io>
2022-04-09 17:23:27 +0200

    [ui] remove perfect-scrollbar + remove navbar on auth page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,3 @@
-/*!
-
-=========================================================
-* Paper Dashboard PRO React - v1.3.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/paper-dashboard-pro-react
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from 'react'
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 
@@ -23,7 +7,7 @@ import AdminLayout from 'layouts/Admin.js'
 import 'bootstrap/dist/css/bootstrap.css'
 import './assets/scss/paper-dashboard.scss'
 import './App.css'
-import 'perfect-scrollbar/css/perfect-scrollbar.css'
+//import 'perfect-scrollbar/css/perfect-scrollbar.css'
 
 export default function App() {
   return (
