68be794a95a5cb8147e869cf8480f88874c65bc1
lts-po <philip.olausson@longterm.io>
2022-04-09 11:02:56 +0200

    [ui] new api

--- frontend/src/views/WirelessConfiguration.js
+++ frontend/src/views/WirelessConfiguration.js
@@ -1,60 +1,50 @@
-import React, { useState, useContext, Component } from 'react'
+import React, { Component } from 'react'
 
-import { hostapdConfiguration } from "components/Helpers/Api.js";
-import {APIErrorContext} from 'layouts/Admin.js';
+import { wifiAPI } from 'api'
+import { APIErrorContext } from 'layouts/Admin'
 
-// reactstrap components
 import {
-  Badge,
-  Button,
   Card,
   CardHeader,
   CardBody,
   CardFooter,
   CardTitle,
-  Label,
-  FormGroup,
-  Input,
-  Table,
   Row,
-  Col,
-  UncontrolledTooltip,
-} from "reactstrap";
+  Col
+} from 'reactstrap'
 
 export default class WirelessConfiguration extends Component {
+  state = { configText: '' }
 
-  state = {configText: ""}
-
-  static contextType = APIErrorContext;
+  static contextType = APIErrorContext
 
   async componentDidMount() {
-
-    hostapdConfiguration().then((data) => {
-      this.setState({configText: data})
-    }).catch((err) => {
-      this.context.reportError("API Failure get traffic: " + err.message)
-    });
-
+    wifiAPI
+      .config()
+      .then((data) => {
+        this.setState({ configText: data })
+      })
+      .catch((err) => {
+        this.context.reportError('API Failure get traffic: ' + err.message)
+      })
   }
 
   render() {
     return (
-        <div className="content">
-          <Row>
-            <Col>
-              <Card>
-                <CardHeader>
-                  <h3>Hostapd Configuration:</h3>
-                </CardHeader>
-                <CardBody>
-                  <pre>
-                  {this.state.configText}
-                  </pre>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-        </div>
-    );
+      <div className="content">
+        <Row>
+          <Col>
+            <Card>
+              <CardHeader>
+                <h3>Hostapd Configuration:</h3>
+              </CardHeader>
+              <CardBody>
+                <pre>{this.state.configText}</pre>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    )
   }
 }
