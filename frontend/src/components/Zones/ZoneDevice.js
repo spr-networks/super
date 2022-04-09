b4a6e25d21aa087058a8ea57750e1fc910ab77bf
lts-po <philip.olausson@longterm.io>
2022-04-09 13:16:21 +0200

    [ui] cleanup

--- frontend/src/components/Zones/ZoneDevice.js
+++ frontend/src/components/Zones/ZoneDevice.js
@@ -1,29 +1,26 @@
 import { Component } from 'react'
 
 export default class ZoneDevice extends Component {
-
   state = {
     zones: []
   }
 
-
   async componentDidMount() {
-
     const setState = (v) => {
       this.setState(v)
     }
   }
 
   render() {
-    const device = this.props.device;
+    const device = this.props.device
 
     return (
-        <tr>
-          <td className=""> {device.Name} </td>
-          <td className=""> {device.MAC }</td>
-          <td className=""> {device.IP } </td>
-          <td className=""> {device.ifname } </td>
-        </tr>
+      <tr>
+        <td>{device.Name}</td>
+        <td>{device.MAC}</td>
+        <td>{device.IP}</td>
+        <td>{device.ifname}</td>
+      </tr>
     )
   }
 }
