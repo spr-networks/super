1f2c0477d262665a4c642af8f60d0d975b6b8ea8
Alex Rad <alex+rad@longterm.io>
2022-04-21 16:54:51 +0000

    [wireguard] use wg-quick on startup

--- wireguard/scripts/startup.sh
+++ wireguard/scripts/startup.sh
@@ -15,11 +15,7 @@ if [ $? -eq 0 ]; then
 	cat $WIREGUARD_CONFIG | sed "s/PrivateKey = privkey/PrivateKey = $ESCAPED_PUBKEY/g" | tee $WIREGUARD_CONFIG
 fi
 
-ip link add dev wg0 type wireguard
-ip addr flush dev wg0
-wg setconf wg0 $WIREGUARD_CONFIG
+wg-quick up $WIREGUARD_CONFIG
 ip link set dev wg0 multicast on
-ip link set dev wg0 up
-
 
 /wireguard_plugin
