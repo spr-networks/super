981fb99301381310a4295ba9166854763523244c
lts-po <philip.olausson@longterm.io>
2022-04-29 14:28:15 +0200

    [api] update script + comments

--- api/scripts/update-zone-to-group.sh
+++ api/scripts/update-zone-to-group.sh
@@ -6,6 +6,12 @@
 
 D="/"
 
+# if run outside of container
+if [ ! -d "$D/configs" ]; then
+	SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+	D="${SCRIPT_DIR}/../.."
+fi
+
 if [ -f "$D/configs/devices/zones.json" ]; then
 	echo "+ updating zone files"
 	mv $D/configs/devices/zones.json $D/configs/devices/zones.json.bak-upgrade
@@ -13,6 +19,5 @@ if [ -f "$D/configs/devices/zones.json" ]; then
 	cat $D/configs/devices/zones.json.bak-upgrade | sed 's/ZoneTags/GroupTags/g' > $D/configs/devices/groups.json
 	cat $D/configs/devices/devices.json.bak-upgrade | sed 's/Zones/Groups/g' > $D/configs/devices/devices.json
 else
-	#echo "+ already updated"
 	exit
 fi
