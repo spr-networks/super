778a4ea5e115e7b24a4ddc9468956b548a6a51fd
lts-po <philip.olausson@longterm.io>
2022-04-29 13:56:15 +0200

    [api] update rename script

--- api/scripts/update-zone-to-group.sh
+++ api/scripts/update-zone-to-group.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+# NOTE
+# after running this restart at least base + api containers and
+# reconnect to wifi to refresh the nft maps
+
 D="/"
 
 if [ -f "$D/configs/devices/zones.json" ]; then
 	echo "+ updating zone files"
+	mv $D/configs/devices/zones.json $D/configs/devices/zones.json.bak-upgrade
+	mv $D/configs/devices/devices.json $D/configs/devices/devices.json.bak-upgrade
+	cat $D/configs/devices/zones.json.bak-upgrade | sed 's/ZoneTags/GroupTags/g' > $D/configs/devices/groups.json
+	cat $D/configs/devices/devices.json.bak-upgrade | sed 's/Zones/Groups/g' > $D/configs/devices/devices.json
 else
 	#echo "+ already updated"
 	exit
 fi
-
-mv $D/configs/devices/zones.json $D/configs/devices/zones.json.bak-upgrade
-mv $D/configs/devices/devices.json $D/configs/devices/devices.json.bak-upgrade
-cat $D/configs/devices/zones.json.bak-upgrade | sed 's/ZoneTags/GroupTags/g' > $D/configs/devices/groups.json
-cat $D/configs/devices/devices.json.bak-upgrade | sed 's/Zones/Groups/g' > $D/configs/devices/devices.json
