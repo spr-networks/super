14bc93fda5a1847a38a765f65d77d7f61caa012e
lts-po <philip.olausson@longterm.io>
2022-04-29 19:54:18 +0200

    [ui] edit hostapd fields

--- frontend/src/components/Wifi/WifiHostapd.js
+++ frontend/src/components/Wifi/WifiHostapd.js
@@ -3,14 +3,40 @@ import { useEffect, useState } from 'react'
 import { wifiAPI } from 'api'
 import { ucFirst } from 'utils'
 
-import { Input, Row, Col } from 'reactstrap'
+import {
+  Button,
+  Form,
+  FormGroup,
+  FormText,
+  Label,
+  Input,
+  Row,
+  Col
+} from 'reactstrap'
 
 const WifiHostapd = (props) => {
   const [config, setConfig] = useState({})
+  const canEdit = ['ssid', 'channel']
+
+  const sortConf = (conf) => {
+    // put the ones we can change at the top
+    return Object.keys(conf)
+      .sort((a, b) => {
+        if (canEdit.includes(a)) {
+          return -1
+        }
+
+        return a > b ? 1 : a < b ? -1 : 0
+      })
+      .reduce((obj, key) => {
+        obj[key] = conf[key]
+        return obj
+      }, {})
+  }
 
   useEffect(() => {
-    wifiAPI.config().then((config) => {
-      setConfig(config)
+    wifiAPI.config().then((conf) => {
+      setConfig(sortConf(conf))
     })
   }, [])
 
@@ -18,35 +44,71 @@ const WifiHostapd = (props) => {
     let name = e.target.name,
       value = e.target.value
 
-    let configNew = config
+    let configNew = { ...config }
     configNew[name] = value
-    console.log(name, '=>', value, configNew)
+
     setConfig(configNew)
-
-    name = ucFirst(name)
-    /*let config = {}
-    config[name] = value
-
-    wifiAPI.updateConfig(config).then((config) => {
-      setConfig(config)
-    })*/
   }
 
-  const canEdit = ['ssid', 'channel']
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    let data = {
+      Ssid: config.ssid,
+      Channel: parseInt(config.channel)
+    }
+
+    wifiAPI.updateConfig(data).then((config) => {
+      setConfig(sortConf(config))
+    })
+  }
 
   return (
-    <>
+    <Form onSubmit={handleSubmit}>
       <Row>
         <Col>
           {Object.keys(config).map((label) => (
-            <dl className="row" key={label}>
-              <dt className="col-sm-3 sm-text-right">{label}</dt>
-              <dd className="col-sm-9">{config[label]}</dd>
-            </dl>
+            <Row key={label}>
+              <Label sm="3" className="sm-text-right">
+                {label}
+              </Label>
+              <Col sm="9">
+                <FormGroup className="row" key={label}>
+                  <Input
+                    autoFocus
+                    type="text"
+                    disabled={!canEdit.includes(label)}
+                    className="col-sm-9"
+                    name={label}
+                    value={config[label]}
+                    onChange={handleChange}
+                  />
+                </FormGroup>
+              </Col>
+            </Row>
           ))}
         </Col>
       </Row>
-    </>
+
+      <p className="text-center text-muted mt-4">
+        <em>NOTE:</em> Editing hostapd.conf requires restarting the Wifi &amp;
+        your connection will be dropped
+      </p>
+
+      <Row className="mt-4">
+        <Col sm={{ offset: 0, size: 12 }} className="text-center">
+          <Button
+            className="btn-wd"
+            color="primary"
+            size="md"
+            type="submit"
+            onClick={handleSubmit}
+          >
+            Save
+          </Button>
+        </Col>
+      </Row>
+    </Form>
   )
 }
 
