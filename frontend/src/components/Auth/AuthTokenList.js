c77b1cb2059f36d9e650b994f1ac775fce1ad0f4
lts-po <philip.olausson@longterm.io>
2022-05-12 18:11:40 +0200

    [ui] new auth settings

---
+++ frontend/src/components/Auth/AuthTokenList.js
@@ -0,0 +1,123 @@
+import { useContext, useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faPlus, faXmark } from '@fortawesome/free-solid-svg-icons'
+
+import {
+  Box,
+  Button,
+  Heading,
+  HStack,
+  Icon,
+  IconButton,
+  Text,
+  View,
+  Fab,
+  FlatList,
+  useColorModeValue
+} from 'native-base'
+
+import { authAPI } from 'api'
+import { AlertContext } from 'AppContext'
+import ModalConfirm from 'components/ModalConfirm'
+
+const AuthTokenList = (props) => {
+  const [status, setStatus] = useState('not configured')
+  const [tokens, setTokens] = useState([])
+
+  const context = useContext(AlertContext)
+
+  useEffect(() => {
+    authAPI
+      .tokens()
+      .then((tokens) => {
+        setTokens(tokens)
+      })
+      .catch((err) => context.error(err))
+  }, [])
+
+  const deleteListItem = (row) => {
+    authAPI
+      .deleteToken(row.Token)
+      .then((done) => {
+        const newData = [...tokens]
+        const prevIndex = tokens.findIndex((t) => t.Token === row.Token)
+        newData.splice(prevIndex, 1)
+        setTokens(newData)
+      })
+      .catch((err) => context.error(err))
+  }
+
+  const handleAddToken = () => {}
+
+  const handleSubmit = (expire) => {
+    console.log('new token:', expire)
+    authAPI
+      .putToken(expire)
+      .then((res) => {})
+      .catch((err) => context.error(err))
+  }
+
+  return (
+    <View mt={4}>
+      <Box
+        bg={useColorModeValue('backgroundCardLight', 'backgroundCardDark')}
+        rounded="md"
+        width="100%"
+        p={4}
+      >
+        <HStack space="1" mb="2">
+          <Heading fontSize="lg">API Tokens</Heading>
+
+          <ModalConfirm
+            type="Expire"
+            defaultValue="0"
+            handleSubmit={handleSubmit}
+            trigger={(triggerProps) => {
+              return (
+                <Button {...triggerProps} marginLeft="auto">
+                  {'Add Token'}
+                </Button>
+              )
+            }}
+          />
+        </HStack>
+        <FlatList
+          data={tokens}
+          renderItem={({ item }) => (
+            <Box
+              borderBottomWidth={1}
+              borderColor="muted.200"
+              _dark={{ borderColor: 'muted.600' }}
+              py={2}
+            >
+              <HStack
+                w="100%"
+                space={3}
+                alignItems="center"
+                justifyContent="stretch"
+              >
+                <Text flex={1}>{item.Token}</Text>
+                <HStack flex={1} space={1}>
+                  <Text color="muted.500">Expire</Text>
+                  <Text>{item.Expire}</Text>
+                </HStack>
+
+                <IconButton
+                  alignSelf="center"
+                  size="sm"
+                  variant="ghost"
+                  colorScheme="secondary"
+                  icon={<Icon as={FontAwesomeIcon} icon={faXmark} />}
+                  onPress={() => deleteListItem(item)}
+                />
+              </HStack>
+            </Box>
+          )}
+          keyExtractor={(item) => item.Token}
+        />
+      </Box>
+    </View>
+  )
+}
+
+export default AuthTokenList
---
+++ frontend/src/components/Auth/WebAuthn.js
@@ -0,0 +1,214 @@
+import { useContext, useState } from 'react'
+
+import {
+  Box,
+  Button,
+  Heading,
+  HStack,
+  Text,
+  View,
+  VStack,
+  useColorModeValue
+} from 'native-base'
+
+import { api } from 'api'
+
+let ApiBaseUrl = api.baseURL
+let TOKEN = ''
+
+// Base64 to ArrayBuffer
+function bufferDecode(value) {
+  return Uint8Array.from(atob(value), (c) => c.charCodeAt(0))
+}
+
+// ArrayBuffer to URLBase64
+function bufferEncode(value) {
+  return btoa(String.fromCharCode.apply(null, new Uint8Array(value)))
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=/g, '')
+}
+
+const register = (e) => {
+  let username = 'admin'
+  let otp = 1024
+
+  return new Promise((resolve, reject) => {
+    fetch(`${ApiBaseUrl}register/?otp=${otp}&username=${username}`)
+      .then((res) => res.json())
+      .then((credentialCreationOptions) => {
+        console.log('creds', credentialCreationOptions)
+
+        TOKEN = credentialCreationOptions.publicKey.extensions['SPR-Bearer']
+
+        credentialCreationOptions.publicKey.challenge = bufferDecode(
+          credentialCreationOptions.publicKey.challenge
+        )
+        credentialCreationOptions.publicKey.user.id = bufferDecode(
+          credentialCreationOptions.publicKey.user.id
+        )
+        if (credentialCreationOptions.publicKey.excludeCredentials) {
+          for (
+            var i = 0;
+            i < credentialCreationOptions.publicKey.excludeCredentials.length;
+            i++
+          ) {
+            credentialCreationOptions.publicKey.excludeCredentials[i].id =
+              bufferDecode(
+                credentialCreationOptions.publicKey.excludeCredentials[i].id
+              )
+          }
+        }
+
+        return navigator.credentials.create({
+          publicKey: credentialCreationOptions.publicKey
+        })
+      })
+      .then((credential) => {
+        console.log('navigator credentials=', credential)
+
+        let attestationObject = credential.response.attestationObject
+        let clientDataJSON = credential.response.clientDataJSON
+        let rawId = credential.rawId
+
+        let body = JSON.stringify({
+          id: credential.id,
+          rawId: bufferEncode(rawId),
+          type: credential.type,
+          response: {
+            attestationObject: bufferEncode(attestationObject),
+            clientDataJSON: bufferEncode(clientDataJSON)
+          }
+        })
+
+        fetch(`${ApiBaseUrl}register/?otp=${otp}&username=${username}`, {
+          method: 'POST',
+          body,
+          headers: { Authorization: `Bearer ${TOKEN}` }
+        })
+          .then((res) => res.json())
+          .then(resolve)
+          .catch(reject)
+      })
+      .catch((error) => {
+        console.log(error)
+        console.log('failed to register ' + username)
+      })
+  })
+}
+
+const login = (e) => {
+  let username = 'admin'
+  return new Promise((resolve, reject) => {
+    fetch(`${ApiBaseUrl}login/?username=${username}`, {
+      headers: { Authorization: 'Bearer ' + TOKEN }
+    })
+      .then((res) => res.json())
+      .then((credentialRequestOptions) => {
+        TOKEN = credentialRequestOptions.publicKey.extensions['SPR-Bearer']
+
+        credentialRequestOptions.publicKey.challenge = bufferDecode(
+          credentialRequestOptions.publicKey.challenge
+        )
+        credentialRequestOptions.publicKey.allowCredentials.forEach(function (
+          listItem
+        ) {
+          listItem.id = bufferDecode(listItem.id)
+        })
+
+        // call into otp provider
+        return navigator.credentials.get({
+          publicKey: credentialRequestOptions.publicKey
+        })
+      })
+      .then((assertion) => {
+        console.log(assertion)
+
+        let authData = assertion.response.authenticatorData
+        let clientDataJSON = assertion.response.clientDataJSON
+        let rawId = assertion.rawId
+        let sig = assertion.response.signature
+        let userHandle = assertion.response.userHandle
+
+        let body = JSON.stringify({
+          id: assertion.id,
+          rawId: bufferEncode(rawId),
+          type: assertion.type,
+          response: {
+            authenticatorData: bufferEncode(authData),
+            clientDataJSON: bufferEncode(clientDataJSON),
+            signature: bufferEncode(sig),
+            userHandle: bufferEncode(userHandle)
+          }
+        })
+
+        fetch(`${ApiBaseUrl}login/?username=${username}`, {
+          method: 'POST',
+          body,
+          headers: { Authorization: `Bearer ${TOKEN}` }
+        })
+          .then((res) => res.json())
+          .then(resolve)
+          .catch(reject)
+      })
+  })
+}
+
+const WebAuthn = (props) => {
+  const [status, setStatus] = useState('not configured')
+
+  const handleClickRegister = () => {
+    register()
+      .then((status) => {
+        setStatus(status)
+      })
+      .catch((error) => {
+        console.error('failed to register: ' + error)
+      })
+  }
+
+  const handleClickLogin = () => {
+    login()
+      .then((status) => {
+        setStatus(status)
+      })
+      .catch((error) => {
+        console.error('failed to login: ' + error)
+      })
+  }
+
+  return (
+    <View>
+      <Box
+        bg={useColorModeValue('backgroundCardLight', 'backgroundCardDark')}
+        rounded="md"
+        width="100%"
+        p={4}
+      >
+        <HStack space="1" mb="2">
+          <Heading fontSize="lg">Webauthn</Heading>
+
+          <Text
+            marginLeft="auto"
+            pt={2}
+            color={status.startsWith('success') ? 'success.500' : 'muted.500'}
+          >
+            {status}
+          </Text>
+        </HStack>
+
+        <Button.Group size="md">
+          <Button colorScheme="primary" onPress={handleClickRegister}>
+            Register Webauthn device
+          </Button>
+
+          <Button colorScheme="secondary" onPress={handleClickLogin}>
+            Verify Webauthn device
+          </Button>
+        </Button.Group>
+      </Box>
+    </View>
+  )
+}
+
+export default WebAuthn
