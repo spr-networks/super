4122bbed901d1ad9a38bf310757ab81ada83a5c8
lts-po <philip.olausson@longterm.io>
2022-05-14 16:20:26 +0200

    [ui] babel jest for tests

--- frontend/src/__tests__/App.js
+++ frontend/src/__tests__/App.js
@@ -1,18 +1,3 @@
-/*import React from 'react'
-import { render, screen } from '@testing-library/react'
-import App from '../App'
-
-describe('App', () => {
-  test('renders without crashing', () => {
-    const { baseElement } = render(<App />)
-    expect(baseElement).toBeDefined()
-  })
-
-  it('renders login form', () => {
-    render(<App />)
-    expect(screen.getByPlaceholderText('Password')).toBeInTheDocument()
-  })
-})*/
 import 'react-native'
 import React from 'react'
 import App from '../App'
