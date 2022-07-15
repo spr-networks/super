7286c90b3a6377342d33d4e8d3a4f72430ad8310
lts-po <po@longterm.io>
2022-07-15 14:49:34 +0200

    [ui] fix dnslog for mobile. missing Promise.allSettled

--- frontend/src/components/DNS/DNSLogHistoryList.js
+++ frontend/src/components/DNS/DNSLogHistoryList.js
@@ -183,7 +183,18 @@ const DNSLogHistoryList = (props) => {
       return setList([])
     }
 
-    Promise.allSettled(
+    // NOTE native dont support Promise.allSettled
+    const allSettled = (promises) => {
+      return Promise.all(
+        promises.map((promise) =>
+          promise
+            .then((value) => ({ status: 'fulfilled', value }))
+            .catch((reason) => ({ status: 'rejected', reason }))
+        )
+      )
+    }
+
+    allSettled(
       filterIps.map(async (ip) => {
         try {
           let list = await logAPI.history(ip)
