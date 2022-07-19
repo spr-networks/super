3f3510986778b1b11187514ce8e7be3668ec863e
lts-po <po@longterm.io>
2022-07-19 16:26:20 +0200

    [ui] remove cypress

--- frontend/cypress/integration/spec.js
+++
@@ -1,8 +0,0 @@
-describe('My First Test', () => {
-  it('visiting index should redirect', () => {
-    cy.visit('/')
-    //cy.contains('type').click()
-    // Should be on a new URL which includes '/auth/login'
-    cy.url().should('include', '/auth/login')
-  })
-})
