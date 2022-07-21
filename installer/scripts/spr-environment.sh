09027fbeda966da677ae49fe7ca6f1a4c0041edc
ltsrad <alex+rad@longterm.io>
2022-07-20 19:55:17 -0700

    [installer] switch to setup screen

--- installer/scripts/spr-environment.sh
+++ installer/scripts/spr-environment.sh
@@ -2,6 +2,4 @@
 # Modify these to change how the first setup will apply
 WANIF=eth0 #modify with your device's outbound interface
 COMPOSE_FILE=docker-compose-prebuilt.yml
-ADMIN_USER=admin
-ADMIN_PASSWORD=spradmin
 #COMPOSE_FILE=docker-compose-src.yml # use this line instead to build SPR from source
