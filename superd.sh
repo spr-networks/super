98cba7935108699f31547dd7f63062cd31338bda
ltsrad <alex+rad@longterm.io>
2022-07-20 19:50:13 -0700

    [installer] Update superd scripts

---
+++ superd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -a
+. /spr-environment.sh
+if [ ! -f "./superd/superd"]; then
+  cd superd
+  DOCKER_BUILDKIT=1 docker build . -t superd
+  docker cp $(docker create --rm superd):/superd ./superd
+  cd ..
+done
+
+./superd/superd &
