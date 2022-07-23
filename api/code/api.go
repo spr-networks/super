31cea78952e0701a5ae407816ec4ecb39c7b0ffa
ltsrad <alex+rad@longterm.io>
2022-07-22 17:31:13 -0700

    [api] Fix regex for config

--- api/code/api.go
+++ api/code/api.go
@@ -1637,9 +1637,9 @@ func setup(w http.ResponseWriter, r *http.Request) {
 	}
 
 	configData := string(data)
-	matchSSID := regexp.MustCompile(`^(SSID_NAME)=(.*)`)
-	matchInterfaceAP := regexp.MustCompile(`^(SSID_INTERFACE)=(.*)`)
-	matchInterfaceUplink := regexp.MustCompile(`^(WANIF)=(.*)`)
+	matchSSID := regexp.MustCompile(`(?m)^(SSID_NAME)=(.*)`)
+	matchInterfaceAP := regexp.MustCompile(`(?m)^(SSID_INTERFACE)=(.*)`)
+	matchInterfaceUplink := regexp.MustCompile(`(?m)^(WANIF)=(.*)`)
 
 	configData = matchSSID.ReplaceAllString(configData, "$1="+conf.SSID)
 	configData = matchInterfaceAP.ReplaceAllString(configData, "$1="+conf.InterfaceAP)
@@ -1661,8 +1661,8 @@ func setup(w http.ResponseWriter, r *http.Request) {
 	}
 
 	configData = string(data)
-	matchSSID = regexp.MustCompile(`^(ssid)=(.*)`)
-	matchInterfaceAP = regexp.MustCompile(`^(interface)=(.*)`)
+	matchSSID = regexp.MustCompile(`(?m)^(ssid)=(.*)`)
+	matchInterfaceAP = regexp.MustCompile(`(?m)^(interface)=(.*)`)
 
 	configData = matchSSID.ReplaceAllString(configData, "$1="+conf.SSID)
 	configData = matchInterfaceAP.ReplaceAllString(configData, "$1="+conf.InterfaceAP)
