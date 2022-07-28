b1d61122738bdd3bc3d5c5604ad06b5de61815aa
ltsrad <alex+rad@longterm.io>
2022-07-28 15:12:08 -0700

    [superd] Rebuild image more frequently

--- superd.sh
+++ superd.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -a
 . /spr-environment.sh
+cd superd
+
+DOCKER_BUILDKIT=1 docker build . -t superd
+docker cp $(docker create --rm superd):/superd ./superd
+cd ..
+
 if [ ! -f "./superd/superd" ]; then
-  cd superd
-  DOCKER_BUILDKIT=1 docker build . -t superd
-  docker cp $(docker create --rm superd):/superd ./superd
-  cd ..
+  echo "[-] Failed to build superd"
+  exit 1
 fi
 
 ./superd/superd &
