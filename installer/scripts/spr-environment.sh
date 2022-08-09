9a4bb41bc870051be5cdb5476ea79fb3261f4217
ltsrad <alex+rad@longterm.io>
2022-08-09 11:08:59 -0700

    [installer] Fix docker yml file reference

--- installer/scripts/spr-environment.sh
+++ installer/scripts/spr-environment.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Modify these to change how the first setup will apply
 WANIF=eth0 #modify with your device's outbound interface
-COMPOSE_FILE=docker-compose-prebuilt.yml
-#COMPOSE_FILE=docker-compose-src.yml # use this line instead to build SPR from source
+COMPOSE_FILE=docker-compose.yml
+#COMPOSE_FILE=docker-compose-virt.yml # use this line instead to use SPR without wifi
+
