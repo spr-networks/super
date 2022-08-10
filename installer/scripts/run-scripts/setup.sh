68a99c83c9e71330cf67d45111422242647e3515
ltsrad <alex+rad@longterm.io>
2022-08-10 13:26:53 -0700

    [installer] Speed up install, only run linux kernel component install later, and dont run dist-upgrade

--- installer/scripts/run-scripts/setup.sh
+++ installer/scripts/run-scripts/setup.sh
@@ -15,12 +15,11 @@ PARTNUM=$(echo $ROOTPART | grep -o -E "[0-9]+")
 growpart $PART $PARTNUM
 resize2fs $ROOTPART
 
-touch /home/spr/.spr-setup-done
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get -y --fix-broken install
-apt-get -y dist-upgrade
-apt-get -y install docker.io docker-compose nftables linux-modules-extra-raspi
+dpkg --configure -a
+apt-get -y install linux-modules-extra-raspi
 
 # disable iptables for  docker
 echo -ne "{\n  \"iptables\": false\n}" > /etc/docker/daemon.json
@@ -35,5 +34,6 @@ done
 
 mv /lib/udev/rules.d/80-net-setup-link.rules /lib/udev/rules.d/80-net-setup-link.rules.bak
 ln -s /dev/null /lib/udev/rules.d/80-net-setup-link.rules
+touch /home/spr/.spr-setup-done
 
 reboot
