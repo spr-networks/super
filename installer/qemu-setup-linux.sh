5741503a36ff9d98385e7ceac009106664146319
ltsrad <alex+rad@longterm.io>
2022-08-11 10:39:29 -0700

    [installer] Fix qemu-img dependancy

--- installer/qemu-setup-linux.sh
+++ installer/qemu-setup-linux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo apt-get install qemu qemu-img binfmt-support qemu-user-static # Install the qemu packages
+sudo apt-get install qemu qemu-utils binfmt-support qemu-user-static # Install the qemu packages
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes # This step will execute the registering scripts
 
