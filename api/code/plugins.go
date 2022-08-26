72cd14f9c35c8225781455149949ce40538df832
ltsrad <alex+rad@longterm.io>
2022-08-26 12:13:40 -0700

    [api] Fix formatting

--- api/code/plugins.go
+++ api/code/plugins.go
@@ -363,7 +363,7 @@ func stopPlusExt(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	http.Error(w, "Plus extension not found: " + name, 404)
+	http.Error(w, "Plus extension not found: "+name, 404)
 
 }
 
@@ -385,7 +385,7 @@ func startPlusExt(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	http.Error(w, "Plus extension not found: " + name, 404)
+	http.Error(w, "Plus extension not found: "+name, 404)
 
 }
 
