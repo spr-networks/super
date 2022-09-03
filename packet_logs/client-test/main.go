39fbff792e6cfcfd5f7f39b5c31036a74638d4d1
lts-po <po@longterm.io>
2022-09-03 12:14:53 +0200

    [logs] update examples to use InDev and OutDev

--- packet_logs/client-test/main.go
+++ packet_logs/client-test/main.go
@@ -1,4 +1,5 @@
 package main
+
 /*
 this is a test client that subscribe to events from ulogd
 and prints out log message depending on log prefix from netfilter
@@ -18,7 +19,7 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"regexp"
+	//"regexp"
 	"strings"
 	"sync"
 	"time"
@@ -41,53 +42,67 @@ type PacketInfo struct {
 	Prefix    string       `json:"Prefix"`
 	Action    string       `json:"Action"`
 	Timestamp time.Time    `json:"Timestamp"`
+	InDev     string       `json:"InDev"`
+	OutDev    string       `json:"OutDev"`
 }
 
 type ConditionEntry struct {
-        Prefix   string `json:"Prefix"`
-        Protocol string `json:"Protocol"`
-        DstIP    string `json:"DstIP"`
-        DstPort  int    `json:"DstPort"`
-        SrcIP    string `json:"SrcIP"`
-        SrcPort  int    `json:"SrcPort"`
-        //DomainNames     []string `json:"DomainNames"`
+	Prefix   string `json:"Prefix"`
+	Protocol string `json:"Protocol"`
+	DstIP    string `json:"DstIP"`
+	DstPort  int    `json:"DstPort"`
+	SrcIP    string `json:"SrcIP"`
+	SrcPort  int    `json:"SrcPort"`
+	//DomainNames     []string `json:"DomainNames"`
 }
 
 func logTraffic(topic string, data string) {
 	//var logEntry map[string]interface{}
 	var logEntry PacketInfo
 	if err := json.Unmarshal([]byte(data), &logEntry); err != nil {
-		  log.Fatal(err)
+		log.Fatal(err)
 	}
 
 	fmt.Printf("## traffic: %v @ %v\n", topic, logEntry.Timestamp)
 
-	jsondata := `{"Prefix": "test", "DomainNames": ["abctest1234.com"] }`
-	var cond ConditionEntry
-	if err := json.Unmarshal([]byte(jsondata), &cond); err != nil {
-		  log.Fatal(err)
+	if logEntry.InDev != "" {
+		fmt.Printf("## InDev: %v\n", logEntry.InDev)
 	}
 
-	if logEntry.UDP != nil && logEntry.DNS != nil && len(logEntry.DNS.Questions) > 0 {
-		//fmt.Printf("## DNS:\n%v\n", logEntry.DNS)
+	if logEntry.OutDev != "" {
+		fmt.Printf("## OutDev: %v\n", logEntry.OutDev)
+	}
 
-		//dump all A *.org domain lookups
-		r, _ := regexp.Compile("([a-z]+).org")
-		matchDomainName := r.MatchString(fmt.Sprintf("%s", logEntry.DNS.Questions[0].Name))
-
-		//log everything
-		//fmt.Printf(">> %s:%s . match=%v\n", logEntry.DNS.Questions[0].Type, logEntry.DNS.Questions[0].Name, matchDomainName)
-
-		// only Type A for now
-		DNSType := "A"
-
-		if fmt.Sprintf("%s", logEntry.DNS.Questions[0].Type) == DNSType && matchDomainName {
-			fmt.Printf(">> type: %s\tname: \"%s\"\n", 
-				logEntry.DNS.Questions[0].Type,
-				logEntry.DNS.Questions[0].Name)
-			//fmt.Printf("%v\n", data)
+	/*
+		jsondata := `{"Prefix": "test", "DomainNames": ["abctest1234.com"] }`
+		var cond ConditionEntry
+		if err := json.Unmarshal([]byte(jsondata), &cond); err != nil {
+			  log.Fatal(err)
 		}
-	}
+	*/
+
+	/*
+		if logEntry.UDP != nil && logEntry.DNS != nil && len(logEntry.DNS.Questions) > 0 {
+			//fmt.Printf("## DNS:\n%v\n", logEntry.DNS)
+
+			//dump all A *.org domain lookups
+			r, _ := regexp.Compile("([a-z]+).org")
+			matchDomainName := r.MatchString(fmt.Sprintf("%s", logEntry.DNS.Questions[0].Name))
+
+			//log everything
+			//fmt.Printf(">> %s:%s . match=%v\n", logEntry.DNS.Questions[0].Type, logEntry.DNS.Questions[0].Name, matchDomainName)
+
+			// only Type A for now
+			DNSType := "A"
+
+			if fmt.Sprintf("%s", logEntry.DNS.Questions[0].Type) == DNSType && matchDomainName {
+				fmt.Printf(">> type: %s\tname: \"%s\"\n",
+					logEntry.DNS.Questions[0].Type,
+					logEntry.DNS.Questions[0].Name)
+				//fmt.Printf("%v\n", data)
+			}
+		}
+	*/
 }
 
 func main() {
@@ -100,7 +115,7 @@ func main() {
 
 	fmt.Println("client connected:", client)
 
-	stream, err := client.SubscribeTopic("nft:lan:out:") // NOTE need to end with :
+	stream, err := client.SubscribeTopic("nft:") // NOTE need to end with :
 	if nil != err {
 		log.Fatal(err)
 	}
@@ -126,12 +141,12 @@ func main() {
 				continue
 			}
 
-			topic = value[0:index-1]
+			topic = value[0 : index-1]
 			value = value[index:len(value)]
 
 			logTraffic(topic, value)
 		}
 	}()
 
-	time.Sleep(60*time.Second)
+	time.Sleep(60 * time.Second)
 }
