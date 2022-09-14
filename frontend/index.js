fabd30a89c3417a1b05a6b88a5d04bce2a8e409f
lts-po <po@longterm.io>
2022-09-14 13:10:44 +0200

    [ui] ignore warning for now, will change in new version of native base

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,9 @@
 // native app entrypoint
-import { AppRegistry } from 'react-native'
+import { AppRegistry, LogBox } from 'react-native'
 import App from './src/App'
 import { name as appName } from './app.json'
+
+// should be fixed in next native-base + use React 18
+LogBox.ignoreLogs(['When server rendering'])
 
 AppRegistry.registerComponent(appName, () => App)
