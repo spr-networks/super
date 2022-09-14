9cca904d4f93f773cc06df68ce8e32c01eaaa696
lts-po <po@longterm.io>
2022-09-14 12:00:33 +0200

    [ui] update to createRoot - react 18

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,9 @@
+// web app entrypoint
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 import App from './App'
 
-/*
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
- * */
-ReactDOM.render(<App />, document.getElementById('root'))
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(<App />)
