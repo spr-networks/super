2e9436a0d360e77ef30c0070ff991b88316ca7cf
lts-po <po@longterm.io>
2022-09-14 12:00:16 +0200

    [ui] SSRProvider for native id warning

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { NativeBaseProvider } from 'native-base'
-
+import { SSRProvider } from '@react-aria/ssr'
 import {
   NativeRouter as Router,
   Route,
   Routes,
-	Navigate
+  Navigate
 } from 'react-router-native'
 import AuthLayout from 'layouts/Auth'
 import AdminLayout from 'layouts/Admin'
@@ -15,24 +15,30 @@ import { theme } from 'Theme'
 
 export default function App() {
   return (
-    <NativeBaseProvider theme={theme}>
-      <Router>
-        <Routes>
-          <Route key="index" path="/" element={<Navigate to="/auth/login" />} />
+    <SSRProvider>
+      <NativeBaseProvider theme={theme}>
+        <Router>
+          <Routes>
+            <Route
+              key="index"
+              path="/"
+              element={<Navigate to="/auth/login" />}
+            />
 
-          <Route key="auth" path="/auth" element={<AuthLayout />}>
-            {routesAuth.map((r) => (
-              <Route key={r.path} path={r.path} element={<r.element />} />
-            ))}
-          </Route>
+            <Route key="auth" path="/auth" element={<AuthLayout />}>
+              {routesAuth.map((r) => (
+                <Route key={r.path} path={r.path} element={<r.element />} />
+              ))}
+            </Route>
 
-          <Route key="admin" path="/admin" element={<AdminLayout />}>
-            {routesAdmin.map((r) => (
-              <Route key={r.path} path={r.path} element={<r.element />} />
-            ))}
-          </Route>
-        </Routes>
-      </Router>
-    </NativeBaseProvider>
+            <Route key="admin" path="/admin" element={<AdminLayout />}>
+              {routesAdmin.map((r) => (
+                <Route key={r.path} path={r.path} element={<r.element />} />
+              ))}
+            </Route>
+          </Routes>
+        </Router>
+      </NativeBaseProvider>
+    </SSRProvider>
   )
 }
