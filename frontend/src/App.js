d3c204e6a0b8048271273333519539a8042ecbe1
lts-po <po@longterm.io>
2022-09-14 12:25:45 +0200

    [ui] cleanup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,6 @@ import AuthLayout from 'layouts/Auth'
 import AdminLayout from 'layouts/Admin'
 import { routesAuth, routesAdmin } from 'routes'
 import { theme } from 'Theme'
-//import './App.css'
 
 export default function App() {
   return (
