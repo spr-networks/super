a7866eaccde732c48331e2b5a07c17c0ab8ae802
lts-po <po@longterm.io>
2022-09-15 15:06:29 +0200

    [ui] use react 17 for now

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,14 @@
 // web app entrypoint
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom'
 
 import App from './App'
 
+/*
+//react 18 - update to this when native-base is using it
+//import { createRoot } from 'react-dom/client'
 const container = document.getElementById('root')
 const root = createRoot(container)
 root.render(<App />)
+*/
+ReactDOM.render(<App />, document.getElementById('root'))
