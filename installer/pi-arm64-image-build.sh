496944347bbcc21b39fab6dd43409cc000b11c76
ltsrad <alex+rad@longterm.io>
2022-12-15 17:18:18 -0800

    [installer] Update go.sh path for the pi iso

--- installer/pi-arm64-image-build.sh
+++ installer/pi-arm64-image-build.sh
@@ -6,4 +6,4 @@ cd installer
 cp ./data/spr.clean.img ./data/spr.img
 ./scripts/resize.sh
 ./scripts/containers.sh
-docker run --privileged -v /dev:/dev -v $PWD/data:/data -v $PWD/scripts:/scripts/ --platform=aarch64 ubuntu /scripts/go.sh
+docker run --privileged -v /dev:/dev -v $PWD/data:/data -v $PWD/scripts:/scripts/ --platform=aarch64 ubuntu /scripts/go-pi.sh
