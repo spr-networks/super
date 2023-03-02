b0a36574a2a302a5b69f5c1d0d662089121c6720
lts-po <po@longterm.io>
2023-03-02 12:28:06 +0100

    [testing] add code for testing api

---
+++ tests/code/README.md
@@ -0,0 +1,13 @@
+# Run tests
+
+```sh
+export API_URL="http://192.168.2.1" # default
+export TOKEN="YWRtaW46YWRtaW4K" # default b64(admin:admin)
+yarn test
+```
+
+# Build
+
+```sh
+yarn install
+```
---
+++ tests/code/test/auth.js
@@ -0,0 +1,15 @@
+const { request, request_auth } = require('../agent')
+
+describe('auth', () => {
+
+	it('should return 401', (done) => {
+		request.get('/status').expect(401, done)
+	})
+
+	it('should accept token', (done) => {
+		request_auth.get('/status')
+			.expect(200)
+			.expect(/Online/, done)
+	})
+
+})
