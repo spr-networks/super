234b558b94fa46b976f4738ae20fec847badd383
lts-po <po@longterm.io>
2023-03-02 12:32:20 +0100

    [testing] format

--- tests/code/test/auth.js
+++ tests/code/test/auth.js
@@ -1,15 +1,14 @@
 const { request, request_auth } = require('../agent')
 
 describe('auth', () => {
+  it('should return 401', (done) => {
+    request.get('/status').expect(401, done)
+  })
 
-	it('should return 401', (done) => {
-		request.get('/status').expect(401, done)
-	})
-
-	it('should accept token', (done) => {
-		request_auth.get('/status')
-			.expect(200)
-			.expect(/Online/, done)
-	})
-
+  it('should accept token', (done) => {
+    request_auth
+      .get('/status')
+      .expect(200)
+      .expect(/Online/, done)
+  })
 })
--- tests/code/test/device.js
+++ tests/code/test/device.js
@@ -2,34 +2,38 @@ const agent = require('../agent')
 const assert = require('assert')
 
 describe('device', () => {
+  it('should add a device', (done) => {
+    let dev = {
+      MAC: 'pending',
+      Name: 'devName',
+      Groups: ['dns', 'wan'],
+      DeviceTags: [],
+      PSKEntry: { Psk: 'password', Type: 'sae' }
+    }
 
-	it('should add a device', (done) => {
-		let dev = {
-			"MAC": "pending", "Name": "devName", "Groups": ["dns","wan"],
-			"DeviceTags": [], "PSKEntry": {"Psk": "password", "Type": "sae"}
-		}
+    agent
+      .put('/device?identity=pending')
+      .send(dev)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        assert.equal(res.body.Name, 'devName')
+        assert.equal(res.body.PSKEntry.Psk, '**', 'password not masked')
 
-		agent.put('/device?identity=pending')
-			.send(dev)
-			.set('Accept', 'application/json')
-			.expect('Content-Type', /json/)
-			.expect(200)
-			.end((err, res) => {
-					assert.equal(res.body.Name, 'devName')
-					assert.equal(res.body.PSKEntry.Psk, '**', 'password not masked')
+        done()
+      })
+  })
 
-					done()
-				})
-	})
+  it('should list devices', (done) => {
+    agent
+      .get('/devices')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        assert(Object.keys(res.body).length > 0, 'missing devices')
 
-	it('should list devices', (done) => {
-		agent.get('/devices')
-			.expect(200)
-			.expect('Content-Type', /json/)
-			.end((err, res) => {
-				assert(Object.keys(res.body).length > 0, 'missing devices')
-
-				done()
-			})
-	})
+        done()
+      })
+  })
 })
