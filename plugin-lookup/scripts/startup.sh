45dd7105501093b54f69bea583563f246bf24b22
lts-po <po@longterm.io>
2023-03-22 11:35:18 +0100

    [plugin-lookup] try download every 1min if no internet on start

--- plugin-lookup/scripts/startup.sh
+++ plugin-lookup/scripts/startup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# download lists on startup
+# download lists on startup. if no internet access, sleep 1 min and try again
+# run plugin when done
 cd /data
+
+while true; do
 
 # ip2asn
 curl -O https://iptoasn.com/data/ip2asn-v4.tsv.gz
@@ -9,5 +12,13 @@ gunzip -f ip2asn-v4.tsv.gz
 
 # oui
 curl -O https://gitlab.com/wireshark/wireshark/-/raw/master/manuf
+
+if [ -f /data/ip2asn-v4.tsv ]; then
+    break
+else
+    sleep 60
+fi
+
+done
 
 /lookup_plugin
