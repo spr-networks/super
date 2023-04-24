3fb02eaf95931d609829c480b028a1edcb181d49
Alex Rad <37549748+lts-rad@users.noreply.github.com>
2023-04-24 16:19:54 -0700

    Update run.sh

--- installer/scripts/run-scripts/run.sh
+++ installer/scripts/run-scripts/run.sh
@@ -5,6 +5,17 @@ set -a
 
 . /spr-environment.sh
 
+# Work-around for USB bug with Ubuntu & TP-Link adapters
+if grep --quiet Raspberry /proc/cpuinfo; then
+  # Reset the bus for TP-Link adapter
+  BUS=$(lsusb | grep TP-Link | grep LAN | awk '{print $2}' | sed 's/^0*//')
+  if [ -n "$BUS" ]; then
+    echo 0 > /sys/bus/usb/devices/${BUS}-1/authorized
+    echo 1 > /sys/bus/usb/devices/${BUS}-1/authorized
+  fi
+fi
+
+
 cd /home/spr/super/
 docker-compose -f $COMPOSE_FILE up -d
 
