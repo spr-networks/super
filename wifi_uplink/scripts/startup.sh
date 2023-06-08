074be18dd48d820127fe592e59170072e5adc295
ltsrad <alex+rad@longterm.io>
2023-06-08 13:48:14 -0700

    [wifi_uplink] Update wifi_uplink run paths and launch script to use wpa.json

--- wifi_uplink/scripts/startup.sh
+++ wifi_uplink/scripts/startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ -f "/configs/wifi_uplink/ifaces" ]; then
-  while read -r iface; do
-    wpa_supplicant -B -i "$iface" -Dnl80211 -c "/configs/wifi_uplink/wpa_$iface.conf"
-  done < "/configs/wifi_uplink/ifaces"
+if [ -f "/configs/wifi_uplink/wpa.json" ]; then
+  while IFS= read -r iface; do
+    wpa_supplicant -B -i "${iface}" -Dnl80211 -c "/configs/wifi_uplink/wpa_${iface}.conf"
+  done < <(jq -r '.WPAs[].Iface' /configs/wifi_uplink/wpa.json)
 fi
-
+  
 sleep inf
