cff33de79bc8daefe00497e39fd5661f1b31e316
lts-po <po@longterm.io>
2023-06-09 15:35:52 +0200

    [ui] configure uplink for interfaces

--- frontend/src/views/UplinkConfiguration.js
+++ frontend/src/views/UplinkConfiguration.js
@@ -15,7 +15,9 @@ import {
   Heading,
   HStack,
   IconButton,
+  Input,
   FlatList,
+  FormControl,
   Modal,
   Menu,
   Stack,
@@ -24,7 +26,10 @@ import {
   VStack,
   ScrollView,
   useDisclose,
-  useColorModeValue
+  useColorModeValue,
+  Select,
+  Radio,
+  Checkbox
 } from 'native-base'
 import Icon from 'FontAwesomeUtils'
 
@@ -34,11 +39,157 @@ import { wifiAPI, api } from 'api'
 import { AlertContext } from 'AppContext'
 import { ucFirst } from 'utils'
 
+import InputSelect from 'components/InputSelect'
+
+let keymgmts = [
+  { value: 'WPA-PSK WPA-PSK-SHA256', label: 'WPA2' },
+  { value: 'WPA-PSK WPA-PSK-SHA256 SAE', label: 'WPA3' }
+]
+
+const UplinkAdd = ({ iface, onSubmit, ...props }) => {
+  const [item, setItem] = useState({})
+  const [ssids, setSSIDs] = useState([])
+  const [optSSIDs, setOptsSSIDs] = useState([])
+
+  const handleChangeSSID = (SSID) => {
+    let ssidItem = ssids.find((item) => item.ssid == SSID)
+    let BSSID = ssidItem ? ssidItem.bssid : ''
+    let KeyMgmt = 'WPA-PSK WPA-PSK-SHA256'
+
+    if (ssidItem.authentication_suites.includes('SAE')) {
+      KeyMgmt = 'WPA-PSK WPA-PSK-SHA256 SAE'
+    }
+
+    setItem({ ...item, SSID, BSSID, KeyMgmt })
+  }
+
+  const scan = (iface) => {
+    setOptsSSIDs([{ label: 'Scanning...', value: null }])
+
+    //set interface up and scan
+    wifiAPI.ipLinkState(iface, 'up').then(
+      wifiAPI.iwScan(iface).then((scanList) => {
+        setSSIDs(scanList)
+      })
+    )
+  }
+
+  useEffect(() => {
+    if (ssids && ssids.length) {
+      setOptsSSIDs(
+        ssids.map((item) => {
+          return { label: item.ssid, value: item.ssid }
+        })
+      )
+    }
+  }, [ssids])
+
+  useEffect(() => {
+    //defaults
+    setItem({
+      Disabled: true,
+      Password: '',
+      SSID: '',
+      KeyMgmt: 'WPA-PSK WPA-PSK-SHA256 SAE',
+      Priority: '1', // TODO ui component for this
+      BSSID: '',
+      Iface: iface
+    })
+
+    // scan on init
+    if (iface != null) {
+      scan(iface)
+    }
+  }, [])
+
+  return (
+    <VStack space={4}>
+      <FormControl>
+        <FormControl.Label>SSID</FormControl.Label>
+        <InputSelect
+          options={optSSIDs}
+          value={item.SSID}
+          onChange={handleChangeSSID}
+        />
+      </FormControl>
+      <FormControl>
+        <FormControl.Label>BSSID</FormControl.Label>
+        <Input
+          variant="underlined"
+          placeholder="BSSID Optional"
+          value={item.BSSID}
+          onChangeText={(BSSID) => setItem({ ...item, BSSID })}
+          autoFocus
+        />
+      </FormControl>
+      <FormControl>
+        <FormControl.Label>Auth</FormControl.Label>
+        <Select
+          selectedValue={item.KeyMgmt}
+          onValueChange={(KeyMgmt) => setItem({ ...item, KeyMgmt })}
+        >
+          {keymgmts.map((opt) => (
+            <Select.Item key={opt.value} label={opt.label} value={opt.value} />
+          ))}
+        </Select>
+      </FormControl>
+      <FormControl>
+        <FormControl.Label>Password</FormControl.Label>
+        <Input
+          variant="underlined"
+          type="password"
+          autoComplete="off"
+          autoCorrect="off"
+          placeholder="Password..."
+          value={item.Password}
+          onChangeText={(Password) => setItem({ ...item, Password })}
+          autoFocus
+        />
+      </FormControl>
+
+      <FormControl>
+        <FormControl.Label>Priority</FormControl.Label>
+        <Input
+          variant="underlined"
+          keyboardType="numeric"
+          autoComplete="off"
+          autoCorrect="off"
+          placeholder="Priority"
+          value={item.Priority}
+          onChangeText={(value) =>
+            setItem({ ...item, Priority: value.replace(/[^\d+]+/, '') })
+          }
+          autoFocus
+        />
+      </FormControl>
+
+      <FormControl>
+        <FormControl.Label>Status</FormControl.Label>
+
+        <Checkbox
+          size="sm"
+          colorScheme="primary"
+          value={!item.Disabled}
+          onChange={(Enabled) => setItem({ ...item, Disabled: !Enabled })}
+        >
+          Enabled
+        </Checkbox>
+      </FormControl>
+
+      <Button colorScheme="primary" onPress={() => onSubmit(item)}>
+        Save
+      </Button>
+    </VStack>
+  )
+}
+
 const UplinkInfo = (props) => {
   const context = useContext(AlertContext)
 
   const [interfaces, setInterfaces] = useState({})
   const [linkIPs, setLinkIPs] = useState({})
+
+  const [iface, setIface] = useState(null)
 
   function isLocalIpAddress(ipAddress) {
     const ipv4Regex = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/
@@ -98,7 +249,7 @@ const UplinkInfo = (props) => {
   //calculate uplink ips
   let uplinks = []
   let links = []
-  let filtered = ["lo", "sprloop", "wg0", "docker0"]
+  let filtered = ['lo', 'sprloop', 'wg0', 'docker0']
   let k = Object.keys(linkIPs)
   k.sort()
   for (let link of k) {
@@ -110,10 +261,14 @@ const UplinkInfo = (props) => {
       interfaces[link] &&
       (interfaces[link].Type == 'Uplink' || interfaces[link].Type == 'Bonded')
     ) {
-      let entry = { Interface: link, IPs: linkIPs[link], Type: interfaces[link].Type }
+      let entry = {
+        Interface: link,
+        IPs: linkIPs[link],
+        Type: interfaces[link].Type
+      }
       uplinks.push(entry)
     } else {
-      let type = "Other"
+      let type = 'Other'
       if (interfaces[link] && interfaces[link].Type) {
         type = interfaces[link].Type
       }
@@ -134,10 +289,29 @@ const UplinkInfo = (props) => {
   )
 
   const moreMenu = (iface) => (
-    <Menu w={190} closeOnSelect={true} trigger={trigger} iface={iface}>
-      <Menu.Item onPress={() => onOpen()}>Modify Interface</Menu.Item>
+    <Menu w={190} closeOnSelect={true} trigger={trigger}>
+      <Menu.Item
+        onPress={() => {
+          setIface(iface)
+          onOpen()
+        }}
+      >
+        Modify Interface
+      </Menu.Item>
     </Menu>
   )
+
+  const onSubmit = (item) => {
+    console.log('save:', item)
+    //TODO verify endpoint is available
+    api
+      .put('/uplink/wifi', { WPAs: [item] })
+      .then((res) => onClose())
+      .catch((err) => {
+        context.error(err)
+        onClose()
+      })
+  }
 
   return (
     <ScrollView h={'100%'}>
@@ -161,6 +335,7 @@ const UplinkInfo = (props) => {
                 p={4}
                 rounded="md"
                 alignItems="center"
+                justifyContent={'space-between'}
                 borderBottomWidth={1}
                 _dark={{
                   bg: 'backgroundCardDark',
@@ -168,32 +343,30 @@ const UplinkInfo = (props) => {
                 }}
                 borderColor="borderColorCardLight"
               >
-                <Text fontWeight="bold" flex={1}>{item.Interface}</Text>
+                <Text flex={1} fontWeight="bold">
+                  {item.Interface}
+                </Text>
                 <Text flex={1}>{item.Type}</Text>
                 <Text flex={1}>{item.IPs}</Text>
-                {moreMenu(item.Interface)}
-              </HStack>
-            )}
-            __ListHeaderComponent={() => (
-              <HStack
-                bg={useColorModeValue(
-                  'backgroundCardLight',
-                  'backgroundCardDark'
-                )}
-                p={3}
-                rounded="md"
-                my={1}
-              >
-                <Text flex={1} fontWeight="bold">
-                  Interface
-                </Text>
-                <Text flex={1} fontWeight="bold">
-                  IP Address
-                </Text>
+                <Box flex={1}>
+                  {item.Interface.match(/^wlan/)
+                    ? moreMenu(item.Interface)
+                    : null}
+                </Box>
               </HStack>
             )}
           />
+        </VStack>
 
+        <HStack p={4}>
+          <Heading fontSize="md">Interfaces</Heading>
+        </HStack>
+        <VStack
+          mx={{ base: 0, md: 4 }}
+          width={{ base: '100%', md: '50%' }}
+          bg={useColorModeValue('backgroundCardLight', 'backgroundCardDark')}
+          _space={2}
+        >
           <FlatList
             data={links}
             renderItem={({ item }) => (
@@ -203,33 +376,16 @@ const UplinkInfo = (props) => {
                 alignItems="center"
                 borderBottomWidth={1}
                 _dark={{
-                  bg: 'backgroundCardDark',
                   borderColor: 'borderColorCardDark'
                 }}
                 borderColor="borderColorCardLight"
               >
-                <Text fontWeight="bold" flex={1}>{item.Interface}</Text>
+                <Text flex={1} fontWeight="bold">
+                  {item.Interface}
+                </Text>
                 <Text flex={1}>{item.Type}</Text>
                 <Text flex={1}>{item.IPs}</Text>
-                {moreMenu}
-              </HStack>
-            )}
-            __ListHeaderComponent={() => (
-              <HStack
-                bg={useColorModeValue(
-                  'backgroundCardLight',
-                  'backgroundCardDark'
-                )}
-                p={3}
-                rounded="md"
-                my={1}
-              >
-                <Text flex={1} fontWeight="bold">
-                  Interface
-                </Text>
-                <Text flex={1} fontWeight="bold">
-                  IP Address
-                </Text>
+                <Box flex={1}></Box>
               </HStack>
             )}
           />
@@ -238,24 +394,14 @@ const UplinkInfo = (props) => {
             <Modal.Content>
               <Modal.CloseButton />
               <Modal.Header fontSize="4xl" fontWeight="bold">
-                Configure interface
+                {iface ? `Configure ${iface}` : 'Configure interface'}
               </Modal.Header>
               <Modal.Body>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
-                quasi cupiditate expedita, ipsa corporis officia totam similique
-                delectus! Debitis esse, ea blanditiis iste enim iure at odit
-                fugiat autem. Accusamus?
+                {iface ? <UplinkAdd iface={iface} onSubmit={onSubmit} /> : null}
               </Modal.Body>
-              <Modal.Footer>
-                <Button colorScheme="blue" mr={1}>
-                  Save
-                </Button>
-                <Button onPress={onClose}>Close</Button>
-              </Modal.Footer>
             </Modal.Content>
           </Modal>
         </VStack>
-
       </VStack>
     </ScrollView>
   )
