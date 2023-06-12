1529af17357322d5bed819e97c5c9be99dfdb644
ltsrad <alex+rad@longterm.io>
2023-06-12 09:05:27 -0700

    [ui] Add UI End Point view

---
+++ frontend/src/components/Firewall/AddEndpoint.js
@@ -0,0 +1,149 @@
+import React, { useContext } from 'react'
+import PropTypes from 'prop-types'
+import { AlertContext } from 'AppContext'
+
+import ClientSelect from 'components/ClientSelect'
+import { firewallAPI } from 'api'
+
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControl,
+  Input,
+  Link,
+  Radio,
+  Stack,
+  HStack,
+  Spinner,
+  Text
+} from 'native-base'
+
+class AddEndpointImpl extends React.Component {
+  state = {
+    RuleName: '',
+    Protocol: 'tcp',
+    IP: '0.0.0.0/0',
+    Port: 'any',
+    Address: ''
+  }
+
+  constructor(props) {
+    super(props)
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(name, value) {
+    //TODO verify IP && port
+    this.setState({ [name]: value })
+  }
+
+  handleSubmit() {
+    let rule = {
+      RuleName: this.state.RuleName,
+      IP: this.state.ip,
+      Address: this.state.Address,
+      Protocol: this.state.Protocol,
+      Port: this.state.Port,
+    }
+
+    firewallAPI
+      .addEndpoint(rule)
+      .then((res) => {
+        if (this.props.notifyChange) {
+          this.props.notifyChange('endpoint')
+        }
+      })
+      .catch((err) => {
+        this.props.alertContext.error('Firewall API Failure', err)
+      })
+  }
+
+  componentDidMount() {}
+
+  render() {
+    let selOpt = (value) => {
+      return { label: value, value }
+    }
+
+    let Protocols = ['tcp', 'udp'].map((p) => {
+      return { label: p, value: p }
+    })
+
+    return (
+      <Stack space={4}>
+        <HStack space={2}>
+          <FormControl flex="2">
+            <FormControl.Label>Rule Name</FormControl.Label>
+            <Input
+              name="RuleName"
+              value={this.state.RuleName}
+              onSubmitEditing={(value) => this.handleChange('RuleName', value)}
+              onChangeText={(value) => this.handleChange('RuleName', value)}
+              onChange={(value) => this.handleChange('RuleName', value)}
+            />
+          </FormControl>
+        </HStack>
+        <HStack space={4}>
+          <FormControl flex="2">
+            <FormControl.Label>IP Address</FormControl.Label>
+            <Input
+              size="md"
+              variant="underlined"
+              name="SrcIP"
+              value={this.state.IP}
+              onChangeText={(value) => this.handleChange('IP', value)}
+            />
+            <FormControl.HelperText>Accepts IP or CIDR</FormControl.HelperText>
+          </FormControl>
+          <FormControl flex="1">
+            <FormControl.Label>Port</FormControl.Label>
+            <Input
+              size="md"
+              variant="underlined"
+              name="Port"
+              value={this.state.Port}
+              onChangeText={(value) => this.handleChange('Port', value)}
+            />
+          </FormControl>
+        </HStack>
+
+        <FormControl>
+          <FormControl.Label>Protocol</FormControl.Label>
+
+          <Radio.Group
+            name="Protocol"
+            defaultValue={this.state.Protocol}
+            accessibilityLabel="Protocol"
+            onChange={(value) => this.handleChange('Protocol', value)}
+          >
+            <HStack space={2}>
+              <Radio value="tcp">tcp</Radio>
+              <Radio value="udp">udp</Radio>
+            </HStack>
+          </Radio.Group>
+        </FormControl>
+
+        <Button color="primary" size="md" onPress={this.handleSubmit}>
+          Save
+        </Button>
+      </Stack>
+    )
+  }
+}
+
+AddEndpointImpl.propTypes = {
+  notifyChange: PropTypes.func
+}
+
+export default function AddEndpoint(props) {
+  let alertContext = useContext(AlertContext)
+  return (
+    <AddEndpointImpl
+      notifyChange={props.notifyChange}
+      alertContext={alertContext}
+    ></AddEndpointImpl>
+  )
+}
---
+++ frontend/src/components/Firewall/EndpointList.js
@@ -0,0 +1,146 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { Icon, FontAwesomeIcon } from 'FontAwesomeUtils'
+import {
+  faArrowRightLong,
+  faCirclePlus,
+  faPlus,
+  faXmark
+} from '@fortawesome/free-solid-svg-icons'
+
+import { firewallAPI, deviceAPI } from 'api'
+import ModalForm from 'components/ModalForm'
+import AddEndpoint from './AddEndpoint'
+
+import {
+  Badge,
+  Button,
+  Box,
+  FlatList,
+  Heading,
+  IconButton,
+  Stack,
+  HStack,
+  VStack,
+  Text,
+  useColorModeValue
+} from 'native-base'
+
+import { FlashList } from '@shopify/flash-list'
+
+const EndpointList = (props) => {
+  const [list, setList] = useState([])
+
+  const refreshList = () => {
+    firewallAPI.config().then((config) => {
+      let flist = config.Endpoints
+      if (flist != null) {
+        setList(flist)
+      }
+    })
+  }
+
+  const deleteListItem = (item) => {
+    firewallAPI.deleteEndpoint(item).then((res) => {
+      refreshList()
+    })
+  }
+
+  useEffect(() => {
+    refreshList()
+  }, [])
+
+  let refModal = useRef(null)
+
+  const notifyChange = (type) => {
+    refModal.current()
+    refreshList()
+  }
+
+  return (
+    <>
+      <HStack justifyContent="space-between" alignItems="center" p={4}>
+        <VStack maxW="60%">
+          <Heading fontSize="md">Endpoints</Heading>
+          <Text color="muted.500" isTruncated>
+            Describe Service Endpoints for building Firewall Rules
+          </Text>
+        </VStack>
+        <ModalForm
+          title="Add Service Endpoint"
+          triggerText="Add Service Endpoint"
+          modalRef={refModal}
+        >
+          <AddEndpoint notifyChange={notifyChange} />
+        </ModalForm>
+      </HStack>
+
+      <Box px={4} mb={4}>
+        <FlatList
+          data={list}
+          renderItem={({ item }) => (
+            <Box
+              bg="backgroundCardLight"
+              borderBottomWidth={1}
+              _dark={{
+                bg: 'backgroundCardDark',
+                borderColor: 'borderColorCardDark'
+              }}
+              borderColor="borderColorCardLight"
+              p={4}
+            >
+              <HStack
+                space={3}
+                justifyContent="space-between"
+                alignItems="center"
+              >
+                <Badge variant="outline">{item.Protocol}</Badge>
+
+                <HStack space={1}>
+                  <Text bold>
+                    {item.IP}
+                  </Text>
+                  <Text color="muted.500">:</Text>
+                  <Text>{item.Port}</Text>
+                </HStack>
+
+                <Icon color="muted.400" icon={faArrowRightLong} />
+
+                <IconButton
+                  alignSelf="center"
+                  size="sm"
+                  variant="ghost"
+                  colorScheme="secondary"
+                  icon={<Icon icon={faXmark} />}
+                  onPress={() => deleteListItem(item)}
+                />
+              </HStack>
+            </Box>
+          )}
+          keyExtractor={(item) =>
+            `${item.Protocol}${item.iP}:${item.Port}`
+          }
+        />
+
+        <VStack>
+          {!list.length ? (
+            <Text alignSelf={'center'}>
+              There are no forward rules configured yet
+            </Text>
+          ) : null}
+          <Button
+            display={{ base: 'flex', md: list.length ? 'none' : 'flex' }}
+            variant={useColorModeValue('subtle', 'solid')}
+            colorScheme="muted"
+            leftIcon={<Icon icon={faCirclePlus} />}
+            onPress={() => refModal.current()}
+            mt={4}
+          >
+            Add Endpoint
+          </Button>
+        </VStack>
+      </Box>
+    </>
+  )
+}
+
+export default EndpointList
