ba9a4a7b1cd27f6c9b9b6ce287848d79fb970ce5
ltsrad <alex+rad@longterm.io>
2023-06-13 22:25:29 -0700

    [base] Rewrite firewall rules

--- base/scripts/nft_rules.sh
+++ base/scripts/nft_rules.sh
@@ -2,7 +2,6 @@
 
 #TBD:
 #
-#- move all WAN stuff to verdict maps to account for multi-wan
 #- create PBR for load balacning across WAN interfaces for outbound  traffic
 #- can F_EST_RELATED be moved past MAC spoof check
 
@@ -38,6 +37,21 @@ nft flush ruleset
 nft -f - << EOF
 
 table inet filter {
+  set uplink_interfaces {
+    type ifname;
+    $(if [ "$WANIF" ]; then echo "elements = { $WANIF }" ; fi )
+  }
+
+  set lan_interfaces {
+    type ifname;
+    $(if [ "$LANIF" ]; then echo "elements = { $LANIF }" ; fi )
+  }
+
+  set dockerifs {
+    type ifname;
+    $(if [ "$DOCKERIF" ]; then echo "elements = { $DOCKERIF }" ; fi )
+  }
+
   # Dynamic maps of clients
   map dhcp_access {
     type ifname . ether_addr: verdict;
@@ -58,7 +72,6 @@ table inet filter {
   map lan_access {
     type ipv4_addr . ifname: verdict;
   }
-
 
   map upstream_tcp_port_drop {
     type inet_service : verdict;
@@ -112,6 +125,7 @@ table inet filter {
     }
   }
 
+  # Forwarding to Endpoint Service definitions
   map ept_udpfwd {
     type ipv4_addr . ipv4_addr . inet_service : verdict ;
     flags interval;
@@ -127,6 +141,15 @@ table inet filter {
     counter drop
   }
 
+  chain LOG_WAN_IN {
+    log prefix "wan:in " group 0
+  }
+
+  chain LOG_LAN_IN {
+    log prefix "lan:in " group 0
+  }
+
+
   chain INPUT {
     type filter hook input priority 0; policy drop;
 
@@ -134,39 +157,38 @@ table inet filter {
     iif lo counter accept
 
     # Mark whether the input came from upstream (wan:in) or local network (lan:in)
-    $(if [ "$WANIF" ]; then echo "iifname $WANIF log prefix \"wan:in \" group 0"; fi)
-    $(if [ "$WANIF" ]; then echo "iifname ne $WANIF log prefix \"lan:in \" group 0"; else echo "log prefix \"lan:in \" group 0"; fi)
+    iifname @uplink_interfaces log prefix "wan:in " group 0
+    iifname != @uplink_interfaces log prefix "lan:in " group 0
 
     # Drop input from the site to site output interfaces. They are only a sink,
     # Not a source that can connect into SPR services
     counter iifname "site*" jump DROPLOGINP
 
-    # Allow wireguard from only WANIF interface to prevent loops
-    $(if [ "$WANIF" ]; then echo "iifname $WANIF udp dport $WIREGUARD_PORT counter accept"; fi)
+    # Allow wireguard from only WANIF interfaces to prevent loops
+    iifname @uplink_interfaces udp dport $WIREGUARD_PORT counter accept
 
     # drop dhcp requests, multicast ports from upstream
     # When updating lan_udp_accept, updated this list.
-    $(if [ "$WANIF" ]; then echo "iifname $WANIF udp dport {67, 1900, 5353} counter jump DROPLOGINP"; fi)
-
+    iifname @uplink_interfaces udp dport {67, 1900, 5353} counter jump DROPLOGINP
 
     # drop ssh, iperf from upstream
-    $(if [ "$WANIF" ]; then echo "counter iifname $WANIF tcp dport vmap @upstream_tcp_port_drop"; fi)
 
     # Extra hardening for API port 80 when running Virtual SPR, to avoid exposing API to the internet
-    $(if [ "$VIRTUAL_SPR_API_INTERNET" ]; then echo "" ;  elif [[ "$WANIF" && "$WAN_NET" ]]; then echo "counter iifname $WANIF tcp dport 80 ip saddr != $WAN_NET drop"; fi)
+    # https://github.com/moby/moby/issues/22054 This is an open issue with docker leaving forwarding open...
+    # Can disable this hardening by setting VIRTUAL_SPR_API_INTERNET=1
+    $(if [ "$VIRTUAL_SPR_API_INTERNET" ]; then echo "" ;  elif [[ "$WAN_NET" ]]; then echo "counter iifname @uplink_interfaces tcp dport 80 ip saddr != $WAN_NET drop"; fi)
 
     counter jump F_EST_RELATED
 
     # DHCP Allow rules
     # Wired lan
-    $(if [ "$LANIF" ]; then echo "iifname $LANIF udp dport 67 counter accept"; fi)
+    iifname @lan_interfaces udp dport 67 counter accept
 
     # Authorized wireless stations & MACs. They do not have an ip address yet
     counter udp dport 67 iifname . ether saddr vmap @dhcp_access
 
     # Prevent MAC Spoofing from LANIF, wired interfaces
-    $(if [ "$LANIF" ]; then echo "iifname eq $LANIF jump DROP_MAC_SPOOF"; fi)
-
+    iifname @lan_interfaces jump DROP_MAC_SPOOF
     jump WIPHY_MACSPOOF_CHECK
 
     # DNS Allow rules
@@ -199,28 +221,40 @@ table inet filter {
     # MSS clamping to handle upstream MTU limitations
     tcp flags syn tcp option maxseg size set rt mtu
 
+    # Block rules
+    counter ip saddr . ip daddr . ip protocol . tcp dport vmap @fwd_block
+    counter ip saddr . ip daddr . ip protocol . udp dport vmap @fwd_block
+
     #jump USERDEF_FORWARD
 
-    counter jump F_EST_RELATED
-
     # mark outbound for upstream with wan:out and others as lan:out
-    $(if [ "$WANIF" ]; then echo "oifname $WANIF log prefix \"wan:out \" group 0"; fi)
-    $(if [ "$WANIF" ]; then echo "oifname ne $WANIF log prefix \"lan:out \" group 0"; else echo "log prefix \"lan:out \" group 0"; fi)
+    oifname @uplink_interfaces log prefix "wan:out " group 0
+    oifname != @uplink_interfaces log prefix "lan:out " group 0
 
     # Extra hardening for when running Virtual SPR, to avoid exposing API to the uplink hop
     # https://github.com/moby/moby/issues/22054 This is an open issue with docker leaving forwarding open...
-    $(if [ "$VIRTUAL_SPR_API_INTERNET" ]; then echo "" ;  elif [[ "$WANIF" && "$WAN_NET" ]]; then echo "counter iifname $WANIF tcp dport 80 ip saddr != $WAN_NET drop"; fi)
+    # Can disable this hardening by setting VIRTUAL_SPR_API_INTERNET=1
+    $(if [ "$VIRTUAL_SPR_API_INTERNET" ]; then echo "" ;  elif [[ "$WANIF" && "$WAN_NET" ]]; then echo "counter iifname @uplink_interfaces tcp dport 80 ip saddr != $WAN_NET drop"; fi)
 
     # Allow DNAT for port forwarding
     counter ct status dnat accept
 
+    # Do not forward from uplink interfaces after dnat
+    iifname @uplink_interfaces drop
+
+    counter jump F_EST_RELATED
+
     # allow docker containers to communicate upstream
-    $(if [ "$WANIF" ] && [ "$DOCKERIF" ]; then echo "iif $DOCKERIF oifname $WANIF ip saddr $DOCKERNET counter accept"; fi)
+    iifname @dockerifs oifname @uplink_interfaces ip saddr $DOCKERNET counter accept
+
+    oifname @uplink_interfaces log prefix "wan:out " group 0
+    oifname != @uplink_interfaces log prefix "lan:out " group 0
+
     # allow docker containers to speak to LAN also
-    $(if [ "$LANIF" ] && [ "$DOCKERIF" ]; then echo "iif $DOCKERIF oifname $LANIF ip saddr $DOCKERNET counter accept"; fi)
+    iifname @dockerifs oifname @lan_interfaces  ip saddr $DOCKERNET counter accept
 
     # Verify MAC addresses for LANIF/WIPHYs
-    $(if [ "$LANIF" ]; then echo "iifname eq $LANIF jump DROP_MAC_SPOOF"; fi)
+    iifname @lan_interfaces jump DROP_MAC_SPOOF
     jump WIPHY_MACSPOOF_CHECK
 
     # After MAC SPOOF check, but before rfc1918 check
@@ -229,24 +263,21 @@ table inet filter {
     counter ip saddr . ip daddr . tcp dport vmap @ept_tcpfwd
 
     # Drop private_rfc1918 access on upstream
-    $(if [ "$WANIF" ]; then echo "counter oifname $WANIF ip daddr vmap @drop_private_rfc1918"; fi)
-
-    # Block rules
-    counter ip saddr . ip daddr . ip protocol . tcp dport vmap @fwd_block
-    counter ip saddr . ip daddr . ip protocol . udp dport vmap @fwd_block
+    counter oifname @uplink_interfaces ip daddr vmap @drop_private_rfc1918
 
     # Forward to Site VPN if client has internet access
     counter oifname "site*" ip saddr . iifname vmap @internet_access
 
-    # Forward to WAN
-    $(if [ "$WANIF" ]; then echo "counter oifname $WANIF ip saddr . iifname vmap @internet_access"; fi)
+    # Forward to uplink interfaces
+    # TBD NOW: needs PBR.
+    counter oifname @uplink_interfaces ip saddr . iifname vmap @internet_access
 
     # The @lan_access dynamic verdict map implements the special LAN group in SPR.
     # It and allows one-way access to all stations, without an explicit relationship by IP,
     #  with a NAT return path (F_EST_RELATED)
 
     # 1. Transmit to the LANIF interface
-    $(if [ "$LANIF" ]; then echo "counter oifname $LANIF ip saddr . iifname vmap @lan_access"; fi)
+    counter oifname @lan_interfaces ip saddr . iifname vmap @lan_access
 
     # 2. Transmit to the wireguard interface
     counter oifname wg0 ip saddr . iifname vmap @lan_access
@@ -256,9 +287,6 @@ table inet filter {
 
     # Custom groups. Managed in firewall.go
     jump CUSTOM_GROUPS
-
-    # Custom services represent one-way relationships, with NAT return path
-    jump CUSTOM_SERVICES
 
     # Fallthrough to log + drop
     counter jump DROPLOGFWD
@@ -277,10 +305,6 @@ table inet filter {
   #}
 
   chain CUSTOM_GROUPS {
-
-  }
-
-  chain CUSTOM_SERVICES {
 
   }
 
@@ -313,8 +337,27 @@ table inet filter {
 }
 
 
-
 table inet nat {
+
+  set uplink_interfaces {
+    type ifname;
+    $(if [ "$WANIF" ]; then echo "elements = { $WANIF }" ; fi )
+  }
+
+  set lan_interfaces {
+    type ifname;
+    $(if [ "$LANIF" ]; then echo "elements = { $LANIF }" ; fi )
+  }
+
+  set dockerifs {
+    type ifname;
+    $(if [ "$DOCKERIF" ]; then echo "elements = { $DOCKERIF }" ; fi )
+  }
+
+  map upstream_supernetworks_drop {
+    type ifname . ipv4_addr : verdict;
+    flags interval;
+  }
 
   map dnat_tcp_ipmap {
     type ipv4_addr . ipv4_addr . inet_service : ipv4_addr;
@@ -370,16 +413,19 @@ table inet nat {
   chain PREROUTING {
     type nat hook prerouting priority -100; policy accept;
 
+    # DROP supernetwork src ips from uplinks
+    counter iifname . ip saddr vmap @upstream_supernetworks_drop
+
     # Block rules
     counter ip saddr . ip daddr . ip protocol  vmap @block
 
     #jump USERDEF_PREROUTING
 
     # DNAT Port Forwarding from Upstream to clients
-    $(if [ "$WANIF" ]; then echo "counter iifname $WANIF dnat ip addr . port to ip daddr . udp dport map @udpfwd"; fi)
-    $(if [ "$WANIF" ]; then echo "counter iifname $WANIF dnat ip addr . port to ip daddr . tcp dport map @tcpfwd"; fi)
-    $(if [ "$WANIF" ]; then echo "counter iifname $WANIF ip protocol udp dnat to ip daddr map @udpanyfwd"; fi)
-    $(if [ "$WANIF" ]; then echo "counter iifname $WANIF ip protocol tcp dnat to ip daddr map @tcpanyfwd"; fi)
+    counter iifname @uplink_interfaces dnat ip addr . port to ip daddr . udp dport map @udpfwd
+    counter iifname @uplink_interfaces dnat ip addr . port to ip daddr . tcp dport map @tcpfwd
+    counter iifname @uplink_interfaces ip protocol udp dnat to ip daddr map @udpanyfwd
+    counter iifname @uplink_interfaces ip protocol tcp dnat to ip daddr map @tcpanyfwd
 
     # Used for DNAT forwarding
     counter ip protocol tcp dnat ip saddr . ip daddr map @dnat_tcp_anymap
@@ -422,7 +468,7 @@ table inet nat {
 
     # Masquerade upstream traffic
 
-    $(if [ "$WANIF" ]; then echo "oifname $WANIF counter masquerade"; fi)
+    oifname @uplink_interfaces counter masquerade
 
     # Masquerade site-to-site VPN
     counter oifname "site*" masquerade
