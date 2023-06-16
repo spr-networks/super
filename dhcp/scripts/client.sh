76a440ab9c439581c4515e2afe4f3e45365abfb4
ltsrad <alex+rad@longterm.io>
2023-06-15 21:45:35 -0700

    [dhcp] Update dhcp client to interpret interfaces.json for uplink interfaces

--- dhcp/scripts/client.sh
+++ dhcp/scripts/client.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 . /configs/base/config.sh
-if [ "$RUN_WAN_DHCP" ]; then
 
+JSON=/configs/base/interfaces.json
+
+if [ "$RUN_WAN_DHCP" ]; then
   # Reset WANIF to original MAC address
   # This is useful for the mesh plugin,
   # which changes the mac address temporarily.
@@ -9,16 +11,35 @@ if [ "$RUN_WAN_DHCP" ]; then
   ip link set dev $WANIF address $(ethtool -P $WANIF | awk '{print $3}')
   ip link set dev $WANIF up
 
-  # workaround for coredhcp bug
-  ip addr add dev $WANIF 192.168.250.250/32
-  /coredhcp_client -d -i $WANIF -v $RUN_WAN_DHCP_IPV
-  ip addr del dev $WANIF 192.168.250.250/32
 
-  # check for an IP, if no IP, run dhclient as a fallback
-  ping 4.2.2.2 -c 1 -W 3
-  ret=$?
-  if [ "$ret" -eq "1" ]
-  then
-    dhclient -i $WANIF
-  fi
+  # will use WANIF if configs/base/interfaces.json is not available
+  names=$(jq -r '.[] | select(.Type == "Uplink" and .Enabled and (.Subtype == null or .Subtype != "pppup") and (.DisableDHCP == null or .DisableDHCP != true)) | .Name' $JSON || echo $WANIF)
+  # Iterate over the array and run dhclient for each name
+  for name in ${names[@]}; do
+      echo "Running coredhcp_client for ${name}"
+
+      /coredhcp_client -d -i ${name} -v ${RUN_WAN_DHCP_IPV}
+
+      # check for an IP, if no IP, run dhclient as a fallback
+      ping -I ${name} 1.1.1.1 -c 1 -W 3
+      ret=$?
+      if [ "$ret" -eq "1" ]
+      then
+        echo "Failed, trying dhclient for ${name}"
+        dhclient -nw -i ${name}
+      fi
+
+  done
+
+  # Handle static IP assignments
+  jq -r '.[] | select(.Type == "Uplink" and .Enabled and .DisableDHCP == true) | "\(.Name) \(.IP) \(.Router)"' $JSON |
+  while IFS= read -r entry; do
+      read -r name ip router <<< "$entry"
+      echo "Set $name with address $ip route $router"
+      # Assign IP address and router.
+      ip addr flush dev $name
+      ip addr add $ip dev $name
+      ip route add 0.0.0.0/0 via $router dev $name
+  done
+
 fi
--- wifi_uplink/scripts/startup.sh
+++ wifi_uplink/scripts/startup.sh
@@ -3,8 +3,7 @@
 if [ -f "/configs/wifi_uplink/wpa.json" ]; then
   while IFS= read -r iface; do
     wpa_supplicant -B -i "${iface}" -Dnl80211 -c "/configs/wifi_uplink/wpa_${iface}.conf"
-    dhclient -nw ${iface}
   done < <(jq -r '.WPAs[] | select(.Enabled == true) | .Iface' /configs/wifi_uplink/wpa.json)
 fi
-  
+
 sleep inf
