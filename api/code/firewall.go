35422e9cf7f279b0f5f88131d512ae898e8ec661
ltsrad <alex+rad@longterm.io>
2023-06-27 17:58:59 -0700

    [api] fix trim suffix

--- api/code/firewall.go
+++ api/code/firewall.go
@@ -244,7 +244,7 @@ func getDefaultGatewayLocked(dev string) (string, error) {
 	stateFile := "/state/dhcp-client/gateway." + dev
 	router, err := ioutil.ReadFile(stateFile)
 	if err == nil && string(router) != "" {
-		return strings.TrimSuffix(string(router)), nil
+		return strings.TrimSuffix(string(router), "\n"), nil
 	}
 
 	//otherwise guess that  the router is at the start as a fallback.
