353267bc61fa6ac1d04d978b952f2767986e7dbe
lts-po <po@longterm.io>
2023-07-13 14:15:20 +0200

    [ui] add swipe up/down

--- frontend/src/components/useSwipe.js
+++ frontend/src/components/useSwipe.js
@@ -1,28 +1,48 @@
 import { useState } from 'react'
 
-export default (input) => {
-  const [touchStart, setTouchStart] = useState(0)
-  const [touchEnd, setTouchEnd] = useState(0)
+export default ({ onSwipedLeft, onSwipedRight, onSwipedUp, onSwipedDown }) => {
+  const [touchStartX, setTouchStartX] = useState(0)
+  const [touchStartY, setTouchStartY] = useState(0)
+  const [touchEndX, setTouchEndX] = useState(0)
+  const [touchEndY, setTouchEndY] = useState(0)
 
   const minSwipeDistance = 50
 
   const onTouchStart = (e) => {
-    setTouchEnd(0)
-    setTouchStart(e.nativeEvent.touches[0].pageX)
+    setTouchEndX(0)
+    setTouchEndY(0)
+    setTouchStartX(e.nativeEvent.touches[0].pageX)
+    setTouchStartY(e.nativeEvent.touches[0].pageY)
   }
 
-  const onTouchMove = (e) => setTouchEnd(e.nativeEvent.touches[0].pageX)
+  const onTouchMove = (e) => {
+    setTouchEndX(e.nativeEvent.touches[0].pageX)
+    setTouchEndY(e.nativeEvent.touches[0].pageY)
+  }
 
   const onTouchEnd = () => {
-    if (!touchStart || !touchEnd) return
-    const distance = touchStart - touchEnd
-    const isLeftSwipe = distance > minSwipeDistance
-    const isRightSwipe = distance < -minSwipeDistance
-    if (isLeftSwipe && input.onSwipedLeft) {
-      input.onSwipedLeft()
+    if (!touchStartX || !touchEndX || !touchStartY || !touchEndY) return
+    const distanceX = touchStartX - touchEndX
+    const distanceY = touchStartY - touchEndY
+    const isLeftSwipe = distanceX > minSwipeDistance
+    const isRightSwipe = distanceX < -minSwipeDistance
+    const isUpSwipe = distanceY > minSwipeDistance
+    const isDownSwipe = distanceY < -minSwipeDistance
+
+    if (isLeftSwipe && !isUpSwipe && !isDownSwipe && onSwipedLeft) {
+      onSwipedLeft()
     }
-    if (isRightSwipe && input.onSwipedRight) {
-      input.onSwipedRight()
+
+    if (isRightSwipe && !isUpSwipe && !isDownSwipe && onSwipedRight) {
+      onSwipedRight()
+    }
+
+    if (isUpSwipe && !isLeftSwipe && !isRightSwipe && onSwipedUp) {
+      onSwipedUp()
+    }
+
+    if (isDownSwipe && !isLeftSwipe && !isRightSwipe && onSwipedDown) {
+      onSwipedDown()
     }
   }
 
