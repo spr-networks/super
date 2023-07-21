ac5ae165d4314df4a53688c603f4609b6bb9befe
lts-po <po@longterm.io>
2023-07-21 13:37:06 +0200

    [ui] IconItem

--- frontend/src/components/IconPicker.js
+++ frontend/src/components/IconPicker.js
@@ -1,46 +1,16 @@
 import React, { useEffect, useState } from 'react'
 
-import Icon from 'FontAwesomeUtils'
+import { HStack, Pressable } from 'native-base'
+
+import IconItem from './IconItem'
 import { BrandIcons } from 'FontAwesomeUtils'
 
-import {
-  faLaptop,
-  faMobileScreen,
-  faDesktop,
-  faNetworkWired,
-  faWifi,
-  faVideo,
-  faEthernet
-} from '@fortawesome/free-solid-svg-icons'
-
-import { HStack, Pressable, useColorModeValue } from 'native-base'
-
-const IconItem = ({ name, data, color, ...props }) => {
-  let Component = data
-
-  //fa icon
-  if (Component?.props?.icon !== undefined) {
-    return <>{Component}</>
-  }
-
-  let size = 12
-
-  return (
-    <Component
-      size={size}
-      color={color || useColorModeValue('blueGray.600', 'blueGray.200')}
-      {...props}
-    />
-  )
-}
-
-export default ({ value, color, onChange, ...props }) => {
-  const [selected, setSelected] = useState(value)
+const IconPicker = ({ value, color, onChange, ...props }) => {
+  const [selected, setSelected] = useState(null)
 
   let icons = []
 
   useEffect(() => {
-    console.log('INIT ICON', value)
     if (value) {
       setSelected(value)
     }
@@ -58,70 +28,44 @@ export default ({ value, color, onChange, ...props }) => {
     'Linux',
     'Microsoft',
     'PlayStation',
+    'RaspberryPi',
     'Synology',
     'Sonos'
   ]
 
-  let faIcons = {
-    Desktop: faDesktop,
-    Ethernet: faEthernet,
-    Laptop: faLaptop,
-    Mobile: faMobileScreen,
-    Video: faVideo,
-    Wifi: faWifi,
-    Wired: faNetworkWired
-  }
-
-  icons = Object.keys(faIcons).map((name) => {
-    return {
-      name,
-      icon: React.createElement(Icon, {
-        icon: faIcons[name],
-        size: 12,
-        color:
-          selected == name && color
-            ? `${color}.400`
-            : useColorModeValue('blueGray.600', 'blueGray.200')
-      })
-    }
-  })
-
-  icons = [
-    ...icons,
-    ...Object.keys(BrandIcons)
-      .map((name) => {
-        return {
-          name,
-          icon: BrandIcons[name]
-        }
-      })
-      .filter(({ name }) => okBrands.includes(name))
+  let faIcons = [
+    'Desktop',
+    'Ethernet',
+    'Laptop',
+    'Mobile',
+    'Video',
+    'Wif',
+    'Wire'
   ]
+
+  icons = [...faIcons, ...okBrands]
 
   return (
     <HStack
       flexWrap={'wrap'}
       justifyContent={{ base: 'space-evenly', md: 'flex-start' }}
     >
-      {icons.map(({ name, icon }) => (
+      {icons.map((name) => (
         <Pressable
           onPress={() => setSelected(name)}
           p={2}
-          opacity={selected == name ? 1 : 0.2}
+          opacity={selected == name ? 1 : 0.5}
         >
           <IconItem
             name={name}
-            data={icon}
-            color={
-              selected == name
-                ? color
-                  ? `${color}.400`
-                  : useColorModeValue('blueGray.200', 'blueGray.600')
-                : ''
-            }
+            color={selected == name && color ? `${color}.400` : 'blueGray.500'}
           />
         </Pressable>
       ))}
     </HStack>
   )
 }
+
+export default IconPicker
+
+export { IconPicker, IconItem }
