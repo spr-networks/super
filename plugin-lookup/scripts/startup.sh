e89ab13dfdb4c279518cfbf0f7485e4e47eb5038
lts-po <po@longterm.io>
2023-08-16 18:49:27 +0200

    [plugin-lookup] wireshark updated manuf location for oui lookups

--- plugin-lookup/scripts/startup.sh
+++ plugin-lookup/scripts/startup.sh
@@ -11,7 +11,13 @@ curl -O https://iptoasn.com/data/ip2asn-v4.tsv.gz
 gunzip -f ip2asn-v4.tsv.gz
 
 # oui
-curl -O https://gitlab.com/wireshark/wireshark/-/raw/master/manuf
+#curl -O https://raw.githubusercontent.com/boundary/wireshark/master/manuf
+# more entries but have no short format for vendor, fix this for go pkg
+curl -O https://www.wireshark.org/download/automated/data/manuf
+grep -q '#\sApple, Inc.' manuf
+if [ $? -ne 0 ]; then
+	sed -i 's/ \{2,\}/\tspr #/g' manuf
+fi
 
 if [ -f /data/ip2asn-v4.tsv ]; then
     break
