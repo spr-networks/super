bb9c7f7095ec89e174b6566dfab43b9eb40209c6
lts-po <po@longterm.io>
2023-10-02 16:52:10 +0200

    [ui] fix files

--- frontend/src/views/Firewall/FirewallSettings.js
+++ frontend/src/views/Firewall/FirewallSettings.js
@@ -2,13 +2,11 @@ import React from 'react'
 import { Text, View, VStack } from 'native-base'
 
 import ICMP from 'components/Firewall/ICMP'
-import Multicast from 'components/Firewall/Multicast'
 
 const FWSettings = (props) => {
   return (
     <VStack>
       <ICMP />
-      <Multicast />
     </VStack>
   )
 }
