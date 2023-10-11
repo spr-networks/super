8ccb762b5f8676c75026ce938d57104c03d8bcc0
ltsrad <alex+rad@longterm.io>
2023-10-11 14:58:15 -0700

    [ui] Reorder wireless information order

--- frontend/src/views/WirelessConfiguration.js
+++ frontend/src/views/WirelessConfiguration.js
@@ -18,11 +18,11 @@ const WirelessConfiguration = (props) => {
   const [routes] = useState([
     {
       key: 'first',
-      title: 'Interfaces'
+      title: 'Clients'
     },
     {
       key: 'second',
-      title: 'Clients'
+      title: 'Interfaces'
     },
     {
       key: 'third',
@@ -40,8 +40,8 @@ const WirelessConfiguration = (props) => {
   }
 
   const renderScene = SceneMap({
-    first: WifiInterfaceList,
-    second: WifiClients,
+    first: WifiClients,
+    second: WifiInterfaceList,
     third: WifiScan,
     fourth: WifiHostapd
   })
