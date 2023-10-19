54064015c7577183ea348c66c90b633a23e9e938
ltsrad <alex+rad@longterm.io>
2023-10-19 13:51:52 -0700

    [dhcp] Add dhclient hook for renewing gateway state

---
+++ dhcp/scripts/dhclient-gateway-hook.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+case "$reason" in
+    BOUND|RENEW)
+        # Commands to run when a lease is obtained or renewed
+        GATEWAY=(echo ${new_routers} | awk '{print $1}' | cut -d ',' -f1 | cut -d ';' -f1)
+        NAME=$interface
+        echo $GATEWAY > /state/dhcp-client/gateway.${NAME}
+        ;;
+    EXPIRE|FAIL)
+        # Commands to run when a lease expires or fails
+        ;;
+    *)
+        # Any other reason
+        ;;
+esac
