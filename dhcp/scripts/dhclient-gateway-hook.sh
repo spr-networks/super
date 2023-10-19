8048bc0b64f6f7a3514515270dae48d4ed5a4b97
ltsrad <alex+rad@longterm.io>
2023-10-19 14:13:06 -0700

    [dhcp_client] Fix typo in hook script

--- dhcp/scripts/dhclient-gateway-hook.sh
+++ dhcp/scripts/dhclient-gateway-hook.sh
@@ -3,7 +3,7 @@
 case "$reason" in
     BOUND|RENEW)
         # Commands to run when a lease is obtained or renewed
-        GATEWAY=(echo ${new_routers} | awk '{print $1}' | cut -d ',' -f1 | cut -d ';' -f1)
+        GATEWAY=$(echo ${new_routers} | awk '{print $1}' | cut -d ',' -f1 | cut -d ';' -f1)
         NAME=$interface
         echo $GATEWAY > /state/dhcp-client/gateway.${NAME}
         ;;
