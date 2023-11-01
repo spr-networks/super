30517e47661f5c749f6f7a79677def02442c5340
lts-po <po@longterm.io>
2023-11-01 17:37:18 +0100

    [ui] use fab for add

--- frontend/src/views/DNS/DNSOverride.js
+++ frontend/src/views/DNS/DNSOverride.js
@@ -15,6 +15,9 @@ import {
   VStack
 } from '@gluestack-ui/themed'
 
+import ModalForm from 'components/ModalForm'
+import DNSAddOverride from 'components/DNS/DNSAddOverride'
+
 const DNSBlock = (props) => {
   const context = useContext(AlertContext)
   const [enabled, setEnabled] = useState(true)
@@ -40,59 +43,99 @@ const DNSBlock = (props) => {
     refreshConfig()
   }, [])
 
+  let refModalAddBlock = React.createRef()
+  let refModalAddPermit = React.createRef()
+
   const notifyChange = async (type) => {
     if (type == 'config') {
       await refreshConfig()
       return
+    } else if (type == 'block') {
+      refModalAddBlock.current()
+    } else if (type == 'permit') {
+      refModalAddPermit.current()
     }
+    refreshConfig()
   }
 
   if (!enabled) {
     return <PluginDisabled plugin="dns" />
   }
 
+  /*let items = [
+    {type: "block", title: 'Block Custom Domain', list: BlockDomains, modal: refModalAddBlock}
+  ]*/
+
   return (
     <View h="$full">
-      <VStack>
-        <DNSOverrideList
-          key="blockdomain"
-          list={BlockDomains}
-          title="Block Custom Domain"
-          notifyChange={notifyChange}
-        />
+      <ScrollView h="$full">
+        <VStack space="sm" pb="$16">
+          <DNSOverrideList
+            list={BlockDomains}
+            title="Block Custom Domain"
+            notifyChange={notifyChange}
+            renderHeader={() => (
+              <ModalForm
+                title="Add Block for Custom Domain"
+                triggerText="Add Block"
+                triggerProps={{
+                  sx: {
+                    '@base': { display: 'none' },
+                    '@md': { display: 'flex' }
+                  }
+                }}
+                modalRef={refModalAddBlock}
+              >
+                <DNSAddOverride type={'block'} notifyChange={notifyChange} />
+              </ModalForm>
+            )}
+          />
 
-        <DNSOverrideList
-          key="allowdomain"
-          list={PermitDomains}
-          title="Permit Domain Override"
-          notifyChange={notifyChange}
-        />
-      </VStack>
+          <DNSOverrideList
+            key="allowdomain"
+            list={PermitDomains}
+            title="Permit Domain Override"
+            notifyChange={notifyChange}
+            renderHeader={() => (
+              <ModalForm
+                title="Add Permit Domain Override"
+                triggerText="Add Permit"
+                triggerProps={{
+                  sx: {
+                    '@base': { display: 'none' },
+                    '@md': { display: 'flex' }
+                  }
+                }}
+                modalRef={refModalAddPermit}
+              >
+                <DNSAddOverride type={'permit'} notifyChange={notifyChange} />
+              </ModalForm>
+            )}
+          />
+        </VStack>
+      </ScrollView>
 
-      {/*
       <Fab
-        size="md"
-        placement="bottom right"
-        isHovered={false}
-        isDisabled={false}
-        isPressed={false}
+        renderInPortal={false}
+        shadow={1}
+        size="sm"
+        onPress={() => refModalAddBlock.current()}
+        bg="$primary600"
       >
         <FabIcon as={AddIcon} mr="$1" />
         <FabLabel>Add Block</FabLabel>
       </Fab>
       <Fab
-        action="secondary"
-        size="md"
-        placement="bottom right"
-        mr="$40"
-        isHovered={false}
-        isDisabled={false}
-        isPressed={false}
+        renderInPortal={false}
+        shadow={1}
+        size="sm"
+        onPress={() => refModalAddPermit.current()}
+        bg="$primary600"
+        mr="$32"
       >
         <FabIcon as={AddIcon} mr="$1" />
         <FabLabel>Add Permit</FabLabel>
       </Fab>
-      */}
     </View>
   )
 }
