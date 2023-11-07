122279d62d92b18507b423457582670a1e88a647
lts-po <po@longterm.io>
2023-11-07 13:00:21 +0100

    [ui] cleanup

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,8 +1,5 @@
 import React from 'react'
 
-//import { NativeBaseProvider, Menu as MenuNB } from nb
-//import { theme } from 'Theme'
-
 import { Menu, MenuItem, MenuItemLabel } from '@gluestack-ui/themed'
 
 const TestMenu = (props) => {
@@ -18,22 +15,11 @@ const TestMenu = (props) => {
       </MenuItem>
     </Menu>
   )
-
-  //<MenuNB {...props}>{props.children}</MenuNB>
-  /*return (
-    <NativeBaseProvider theme={theme}>
-      <MenuNB trigger={props.trigger}>
-        <MenuNB.Item>Hello</MenuNB.Item>
-        <MenuNB.Item>Hello23</MenuNB.Item>
-      </MenuNB>
-    </NativeBaseProvider>
-  )*/
 }
 
 //Menu, Menu.Item
 TestMenu.Item = MenuItem
 
-//let MenuExport = TestMenu
 let MenuExport = Menu
 
 export default MenuExport
