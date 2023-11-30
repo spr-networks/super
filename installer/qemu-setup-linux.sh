52a672125a07b2a43bb7ea2ecb54efbe402a7c40
Alex Radocea <alex@supernetworks.org>
2023-11-30 14:33:36 -0800

    [build] Fix installer script dependency

--- installer/qemu-setup-linux.sh
+++ installer/qemu-setup-linux.sh
@@ -2,5 +2,5 @@
 sudo apt-get update --fix-missing #try to fix azure ubuntu issue
 sudo apt-get install -y qemu qemu-utils binfmt-support qemu-user-static # Install the qemu packages
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes # This step will execute the registering scripts
-sudo apt-get installl -y cloud-guest-utils
+sudo apt-get install -y cloud-guest-utils
 
