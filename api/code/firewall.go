77921febfac4dcdfbb46b399bb47faeac89c4200
Alex Radocea <alex@supernetworks.org>
2023-12-18 10:21:42 -0800

    [api] Fix firewall bug with SrcPort setting

--- api/code/firewall.go
+++ api/code/firewall.go
@@ -1491,9 +1491,6 @@ func modifyForwardRules(w http.ResponseWriter, r *http.Request) {
 	if fwd.SrcPort != "any" && !re.MatchString(fwd.SrcPort) {
 		http.Error(w, "Invalid SrcPort", 400)
 		return
-	} else {
-		//convert to full range
-		fwd.SrcPort = "0-65535"
 	}
 
 	if fwd.DstPort != "any" {
