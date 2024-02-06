fbf74c3090dc422bf9bc70413f6c153f7ff27f1e
lts-po <po@longterm.io>
2024-02-06 15:14:35 +0100

    [ui] fix eventTemplate for ios, only build template strings

--- frontend/src/components/Alerts/AlertUtil.js
+++ frontend/src/components/Alerts/AlertUtil.js
@@ -1,13 +1,10 @@
 import React from 'react'
+import { Platform } from 'react-native'
 import DeviceItem from 'components/Devices/DeviceItem'
 
 import { InterfaceItem } from 'components/TagItem'
 
-import {
-  HStack,
-  Text,
-} from '@gluestack-ui/themed'
-
+import { HStack, Text } from '@gluestack-ui/themed'
 
 export const transformTag = (context, tag, value) => {
   if (context) {
@@ -19,7 +16,7 @@ export const transformTag = (context, tag, value) => {
     }
 
     let deviceItem = undefined
-    if (tag.startsWith("Device")) {
+    if (tag.startsWith('Device')) {
       deviceItem = context.getDevice(value, type)
 
       if (deviceItem === undefined) {
@@ -27,94 +24,89 @@ export const transformTag = (context, tag, value) => {
       }
     }
 
-    if (tag == "Interface") {
+    if (tag == 'Interface') {
       return <InterfaceItem name={value} />
-    } else if (tag == "Device") {
-      return <DeviceItem
-        show={['Style', 'Name']}
-        flex={1}
-        item={deviceItem}
-      />
-    } else if (tag == "DeviceIcon") {
-      return <DeviceItem
-        show={['Style']}
-        flex={1}
-        item={deviceItem}
-      />
-    } else if (tag == "DeviceName") {
-      return <DeviceItem
-        show={['Name']}
-        flex={1}
-        item={deviceItem}
-      />
-    } else if (tag == "DeviceIP") {
-      return <DeviceItem
-        show={['RecentIP']}
-        flex={1}
-        item={deviceItem}
-      />
-    } else if (tag == "DeviceMAC") {
-      return <DeviceItem
-        show={['MAC']}
-        flex={1}
-        item={deviceItem}
-      />
+    } else if (tag == 'Device') {
+      return <DeviceItem show={['Style', 'Name']} flex={1} item={deviceItem} />
+    } else if (tag == 'DeviceIcon') {
+      return <DeviceItem show={['Style']} flex={1} item={deviceItem} />
+    } else if (tag == 'DeviceName') {
+      return <DeviceItem show={['Name']} flex={1} item={deviceItem} />
+    } else if (tag == 'DeviceIP') {
+      return <DeviceItem show={['RecentIP']} flex={1} item={deviceItem} />
+    } else if (tag == 'DeviceMAC') {
+      return <DeviceItem show={['MAC']} flex={1} item={deviceItem} />
     }
   }
 
-  return value + "#" + tag
+  return (
+    <Text>
+      {value}#{tag}
+    </Text>
+  )
 }
 
 export const eventTemplate = (context, template, event) => {
   if (!template || !event) {
-    return null;
+    return null
   }
 
-  let elements = [];
-  let lastIndex = 0;
+  let elements = []
+  let lastIndex = 0
 
-  template.replace(/\{\{([\w\.]+)(?:#(\w+))?\}\}/g, (match, path, tag, index) => {
-    // Add the text before the match.
-    if (index > lastIndex) {
-      elements.push(template.slice(lastIndex, index));
-    }
+  const supportTags = Platform.OS == 'web'
 
-    if (match.includes("__")) {
-      // Disable double underscore matches.
-      lastIndex = index + match.length;
-      return "";
-    }
-
-    const levels = path.split('.');
-    let currentValue = event;
-    for (let level of levels) {
-      if (currentValue && currentValue[level]) {
-        currentValue = currentValue[level];
-      } else {
-        currentValue = "";
-        break;
+  template.replace(
+    /\{\{([\w\.]+)(?:#(\w+))?\}\}/g,
+    (match, path, tag, index) => {
+      // Add the text before the match.
+      if (index > lastIndex) {
+        elements.push(template.slice(lastIndex, index))
       }
-    }
 
-    if (tag) {
-      elements.push(transformTag(context, tag, currentValue));
-    } else {
-      elements.push(currentValue);
-    }
+      if (match.includes('__')) {
+        // Disable double underscore matches.
+        lastIndex = index + match.length
+        return ''
+      }
 
-    lastIndex = index + match.length;
-    return "";
-  });
+      const levels = path.split('.')
+      let currentValue = event
+      for (let level of levels) {
+        if (currentValue && currentValue[level]) {
+          currentValue = currentValue[level]
+        } else {
+          currentValue = ''
+          break
+        }
+      }
+
+      if (tag && supportTags) {
+        elements.push(transformTag(context, tag, currentValue))
+      } else {
+        elements.push(currentValue)
+      }
+
+      lastIndex = index + match.length
+      return ''
+    }
+  )
 
   // Add any remaining text after the last match.
   if (lastIndex < template.length) {
-    elements.push(template.slice(lastIndex));
+    elements.push(template.slice(lastIndex))
   }
 
-  return (
-    <HStack>
-    {elements.map((element, idx) => (
-      <React.Fragment key={idx}>{element}</React.Fragment>
-    ))}</HStack>
-  );
-};
+  if (supportTags) {
+    return (
+      <>
+        {elements.map((element, idx) => (
+          <React.Fragment key={idx}>{element}</React.Fragment>
+        ))}
+      </>
+    )
+  }
+
+  // return string
+  return elements.join('')
+}
