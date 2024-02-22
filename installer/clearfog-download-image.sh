0ec88a7cd8796a02139ac67dd718998dd94fc1ed
Alex Radocea <alex@supernetworks.org>
2024-02-21 20:19:27 -0800

    [build] [clearfog] Update to mantic base image & kernel

--- installer/clearfog-download-image.sh
+++ installer/clearfog-download-image.sh
@@ -6,7 +6,7 @@ mkdir data
 cd ./data
 
 IMG=ubuntu-cn9130-cf-pro-mmc.1.1.img
-TAG=spr-clearfog
+TAG=spr-clearfog-mantic
 if [ ! -f $IMG ]; then
   wget -q "https://github.com/spr-networks/cn913x_build/releases/download/${TAG}/${IMG}"
 fi
