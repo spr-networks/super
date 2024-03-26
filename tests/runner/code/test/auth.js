b219978864c58d7abab095bbc89f6902e8c1ec93
Alex Radocea <alex@supernetworks.org>
2024-03-26 13:13:12 -0700

    [testing] fix tests deps and 401

--- tests/runner/code/test/auth.js
+++ tests/runner/code/test/auth.js
@@ -3,8 +3,9 @@ const { request, request_auth } = require('../agent')
 describe('auth', () => {
   it('should return 401', (done) => {
     request.get('/status').expect(401, (err, res) => {
-      assert(res.status == 200)
-      done())
+      assert(res.status == 401)
+      done()
+    })
   })
 
   it('should accept token', (done) => {
