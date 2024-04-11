90f30837d9bf3d4e4664488123f853dfa16f64d9
Alex Radocea <alex@supernetworks.org>
2024-04-10 21:31:09 -0700

    [installer] Add missing containers

--- installer/pi-arm64-image-build.sh
+++ installer/pi-arm64-image-build.sh
@@ -11,5 +11,5 @@ fi
 cd installer
 cp ./data/spr.clean.img ./data/spr.img
 ./scripts/resize.sh
-#./scripts/containers.sh
+./scripts/containers.sh
 docker run --privileged -v /dev:/dev -v $PWD/data:/data -v $PWD/scripts:/scripts/ --platform=aarch64 ubuntu:23.10 /scripts/go-pi.sh
