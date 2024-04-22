24b89e6dff03f5b9b93a7c3c12a7441eb4f5eefb
Alex Radocea <alex@supernetworks.org>
2024-04-22 14:25:21 -0700

    [ui] Add vpn connection template

--- frontend/src/components/Alerts/AlertTemplates.js
+++ frontend/src/components/Alerts/AlertTemplates.js
@@ -152,5 +152,25 @@ export default [
     Name: 'dns resolve',
     Disabled: true,
     RuleId: 'f6bdb6ee-ffcb-41af-b3c7-6270cba936fb'
-  }
+  },
+  {
+   TopicPrefix: "device:vpn:online",
+   MatchAnyOne: false,
+   InvertRule: false,
+   Conditions: [],
+   Actions: [
+    {
+     SendNotification: true,
+     StoreAlert: false,
+     MessageTitle: "{{DeviceIP#Device}} connected via {{VPNType}} from {{RemoteEndpoint}}",
+     MessageBody: "{{DeviceIP#Device}} connected via {{VPNType}} from {{RemoteEndpoint}}",
+     NotificationType: "info",
+     GrabEvent: true,
+     GrabValues: false
+    }
+   ],
+   Name: "VPN Connection",
+   Disabled: false,
+   RuleId: "6d3c9a04-8ad4-4ad3-8e7d-4fe61fd3c592"
+ },
 ]
