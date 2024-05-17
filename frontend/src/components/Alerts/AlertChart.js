fa8f98ca748318e64f41ea7b85cdf4f209ce637b
lts-po <po@longterm.io>
2024-05-17 11:23:00 +0200

    [ui] Bring back in mobile view fixes

--- frontend/src/components/Alerts/AlertChart.js
+++ frontend/src/components/Alerts/AlertChart.js
@@ -2,12 +2,7 @@ import React, { useContext } from 'react'
 
 //empty for iOS, default
 const AlertChart = (props) => {
-<<<<<<< HEAD
-  return (
-  )
-=======
   return <></>
->>>>>>> dev
 }
 
 export default AlertChart
--- frontend/src/components/Devices/Device.js
+++ frontend/src/components/Devices/Device.js
@@ -317,6 +317,19 @@ const Device = React.memo(({ device, notifyChange, showMenu, ...props }) => {
               }}
             >
               <HStack space="sm" alignItems="center">
+                <Tooltip label={wifi_type}>
+                  <IconItem
+                    name={wifi_type == 'Wired' ? 'Wire' : 'Wifi'}
+                    size={16}
+                    color={
+                      device.isConnected
+                        ? '$green600'
+                        : colorMode == 'light'
+                        ? '$muted300'
+                        : '$muted700'
+                    }
+                  />
+                </Tooltip>
 
                 <Text size="md" bold>
                   {device.RecentIP}
