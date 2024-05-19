7d0ec7aff43a49fdef15070600d898cf4b2d2bdd
Alex Radocea <alex@supernetworks.org>
2024-05-19 11:03:39 -0700

    [installer] Update to dhcpcd as fallback for 24.04

--- installer/scripts/run-scripts/run.sh
+++ installer/scripts/run-scripts/run.sh
@@ -38,7 +38,7 @@ ret=$?
 
 if [ "$ret" -ne "0" ]; then
    # upon failure, run dhclient to get an IP and try to pull the containers
-   dhclient $WANIF
+   dhcpcd $WANIF || dhclient $WANIF
    docker-compose -f $COMPOSE_FILE pull
    docker-compose -f $COMPOSE_FILE up -d
 fi
