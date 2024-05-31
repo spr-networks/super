3eea254e06455bca2986940fa8c9df0632c1f4a8
Alex Rad <alex@supernetworks.org>
2024-05-30 23:19:54 -0700

    [build] Shrink pi images again

--- installer/scripts/unmount.sh
+++ installer/scripts/unmount.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 if [ $UID != 0 ]; then
 	sudo $0
 	exit
@@ -12,12 +11,16 @@ umount /mnt/fs
 
 IMG="./data/spr.img"
 LOOP=$(losetup -j $IMG | cut -d: -f1)
-
-# disable resizing for now
-# shrink back img 
-e2fsck -f ${LOOP}p2
-#resize2fs ${LOOP}p2 9G
-#e2fsck -f ${LOOP}p2
-
 losetup -d $LOOP 2>/dev/null
-#truncate -s 10G $IMG
+
+# as a hack, we do losetup again
+losetup -Pf $IMG
+LOOP=$(losetup -j $IMG | cut -d: -f1)
+# fsck
+e2fsck -f ${LOOP}p2
+# resize
+resize2fs ${LOOP}p2 4G
+losetup -d $LOOP
+
+# truncate image
+truncate -s 5 $IMG
