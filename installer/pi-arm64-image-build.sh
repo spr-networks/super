d271e8495146717b8c8e26eb0ab1a3cc438d87b9
Alex Rad <alex@supernetworks.org>
2024-05-31 00:30:25 -0700

    [build] Additional fixes for pull syntax

--- installer/pi-arm64-image-build.sh
+++ installer/pi-arm64-image-build.sh
@@ -11,10 +11,10 @@ cd installer
 cp ./data/spr.clean.img ./data/spr.img
 ./scripts/resize.sh
 # pull default containers and default plugins
-DOCKER_DEFAULT_PLATFORM=linux/arm64 docker-compose pull -f docker-compose.yml  -f dyndns/docker-compose.yml -f ppp/docker-compose.yml -f wifi_uplink/docker-compose.yml pull
+DOCKER_DEFAULT_PLATFORM=linux/arm64 docker-compose -f docker-compose.yml  -f dyndns/docker-compose.yml -f ppp/docker-compose.yml -f wifi_uplink/docker-compose.yml pull
 ./scripts/containers.sh
 #use host for next ubuntu
-DOCKER_DEFAULT_PLATFORM="" docker-compose pull ubuntu:24.04
+DOCKER_DEFAULT_PLATFORM="" docker pull ubuntu:24.04
 docker run --privileged -v /dev:/dev -v $PWD/data:/data -v $PWD/scripts:/scripts/ ubuntu:24.04 /scripts/go-pi.sh
 docker run --privileged -v /dev:/dev -v $PWD/data:/data -v $PWD/scripts:/scripts/ --platform=aarch64 ubuntu:24.04 /scripts/go-pi-target.sh
 ./scripts/shrink.sh
