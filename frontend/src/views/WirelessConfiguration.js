a1e1baa7d097d5fc159c117fdba289c82a6cd564
lts-po <po@longterm.io>
2024-06-01 13:20:48 +0200

    [ui] use TabView component

--- frontend/src/views/WirelessConfiguration.js
+++ frontend/src/views/WirelessConfiguration.js
@@ -1,131 +1,27 @@
-import React, { useContext, useState } from 'react'
+import React from 'react'
 
-import { AlertContext } from 'layouts/Admin'
 import WifiClients from 'components/Wifi/WifiClients'
 import WifiInterfaceList from 'components/Wifi/WifiInterfaceList'
 import WifiScan from 'components/Wifi/WifiScan'
 import WifiHostapd from 'components/Wifi/WifiHostapd'
 
-import { Animated, Dimensions, Platform } from 'react-native'
-import { TabView, SceneMap } from 'react-native-tab-view'
+import { Platform } from 'react-native'
 
-import { Box, View, Pressable, useColorMode } from '@gluestack-ui/themed'
+import TabView from 'components/TabView'
 
 const WirelessConfiguration = (props) => {
-  const [index, setIndex] = useState(0) //1)
-
-  const context = useContext(AlertContext)
-
-  const [routes] = useState([
-    {
-      key: 'first',
-      title: 'Clients'
-    },
-    {
-      key: 'second',
-      title: 'Interfaces'
-    },
-    {
-      key: 'third',
-      title: 'Scan'
-    },
-    {
-      key: 'fourth',
-      title: Platform.OS === 'web' ? 'Radio Settings' : 'Settings'
-    }
-  ])
-
-  const initialLayout = {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height
-  }
-
-  const renderScene = SceneMap({
-    first: WifiClients,
-    second: WifiInterfaceList,
-    third: WifiScan,
-    fourth: WifiHostapd
-  })
-
-  const renderTabBar = (props) => {
-    const inputRange = props.navigationState.routes.map((x, i) => i)
-    return (
-      <Box flexDirection="row">
-        {props.navigationState.routes.map((route, i) => {
-          const opacity = props.position.interpolate({
-            inputRange,
-            outputRange: inputRange.map((inputIndex) =>
-              inputIndex === i ? 1 : 0.5
-            )
-          })
-
-          const colorMode = useColorMode()
-          const color =
-            index === i
-              ? colorMode == 'light'
-                ? '#000'
-                : '#e5e5e5'
-              : colorMode == 'light'
-              ? '#1f2937'
-              : '#a1a1aa'
-          const borderColor =
-            index === i
-              ? '$cyan500'
-              : colorMode == 'light'
-              ? '$coolGray200'
-              : '$gray400'
-          return (
-            <Box
-              key={route.title}
-              borderBottomWidth={3}
-              borderColor={borderColor}
-              flex={1}
-              alignItems="center"
-              px="$2"
-              py="$4"
-              cursor="pointer"
-            >
-              <Pressable
-                onPress={() => {
-                  setIndex(i)
-                }}
-              >
-                <Animated.Text
-                  style={{
-                    color
-                  }}
-                >
-                  {route.title}
-                </Animated.Text>
-              </Pressable>
-            </Box>
-          )
-        })}
-      </Box>
-    )
-  }
-
-  // also have the tabs
-  let navbarHeight = 64
-  let tabsHeight = 32
-  let heightContent =
-    Platform.OS == 'web'
-      ? Dimensions.get('window').height - navbarHeight
-      : '100%'
-
   return (
-    <View h={heightContent}>
-      <TabView
-        navigationState={{
-          index,
-          routes
-        }}
-        renderScene={renderScene}
-        renderTabBar={renderTabBar}
-        onIndexChange={setIndex}
-        initialLayout={initialLayout}
-      />
-    </View>
+    <TabView
+      tabs={[
+        { title: 'Clients', component: WifiClients },
+        { title: 'Interfaces', component: WifiInterfaceList },
+        { title: 'Scan', component: WifiScan },
+        {
+          title: Platform.OS === 'web' ? 'Radio Settings' : 'Settings',
+          component: WifiHostapd
+        }
+      ]}
+    />
   )
 }
 
