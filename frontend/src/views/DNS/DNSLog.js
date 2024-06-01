21ac86ff8d4a9b10bcccd5bd6455fbd28256d5b4
lts-po <po@longterm.io>
2024-06-01 13:17:20 +0200

    [ui] use TabView component

--- frontend/src/views/DNS/DNSLog.js
+++ frontend/src/views/DNS/DNSLog.js
@@ -8,10 +8,7 @@ import DNSLogEdit from 'views/DNS/DNSLogEdit'
 import PluginDisabled from 'views/PluginDisabled'
 import { logAPI } from 'api/DNS'
 
-import { Animated, Dimensions, Platform } from 'react-native'
-import { TabView, SceneMap } from 'react-native-tab-view'
-
-import { Box, View, Pressable, useColorMode } from '@gluestack-ui/themed'
+import TabView from 'components/TabView'
 
 const DNSLog = (props) => {
   const [isEnabled, setIsEnabled] = useState(true)
@@ -50,115 +47,14 @@ const DNSLog = (props) => {
 }
 
 const DNSTabView = (props) => {
-  const [index, setIndex] = useState(0) //1)
-  const [routes] = useState([
-    {
-      key: 'first',
-      title: 'DNS Log'
-    },
-    {
-      key: 'second',
-      title: 'Log Settings'
-    }
-    /*    ,
-    {
-      key: 'third',
-      title: 'Graph'
-    }
-*/
-  ])
-
-  const initialLayout = {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height
-  }
-
-  const renderScene = SceneMap({
-    first: DNSLog,
-    second: DNSLogEdit
-    //    third: DNSChart
-  })
-
-  const renderTabBar = (props) => {
-    const inputRange = props.navigationState.routes.map((x, i) => i)
-    return (
-      <Box flexDirection="row">
-        {props.navigationState.routes.map((route, i) => {
-          const opacity = props.position.interpolate({
-            inputRange,
-            outputRange: inputRange.map((inputIndex) =>
-              inputIndex === i ? 1 : 0.5
-            )
-          })
-
-          const colorMode = useColorMode()
-          const color =
-            index === i
-              ? colorMode == 'light'
-                ? '#000'
-                : '#e5e5e5'
-              : colorMode == 'light'
-              ? '#1f2937'
-              : '#a1a1aa'
-          const borderColor =
-            index === i
-              ? '$cyan500'
-              : colorMode == 'light'
-              ? '$coolGray200'
-              : '$gray400'
-          return (
-            <Box
-              key={route.title}
-              borderBottomWidth={3}
-              borderColor={borderColor}
-              flex={1}
-              alignItems="center"
-              px="$2"
-              py="$4"
-              cursor="pointer"
-            >
-              <Pressable
-                onPress={() => {
-                  setIndex(i)
-                }}
-              >
-                <Animated.Text
-                  style={{
-                    color
-                  }}
-                >
-                  {route.title}
-                </Animated.Text>
-              </Pressable>
-            </Box>
-          )
-        })}
-      </Box>
-    )
-  }
-
-  if (Platform.OS == 'web') {
-    return (
-      <View h="$full">
-        <TabView
-          navigationState={{
-            index,
-            routes
-          }}
-          renderScene={renderScene}
-          renderTabBar={renderTabBar}
-          onIndexChange={setIndex}
-          initialLayout={initialLayout}
-        />
-      </View>
-    )
-  } else {
-    return (
-      <View>
-        <DNSLog {...props} />
-      </View>
-    )
-  }
+  return (
+    <TabView
+      tabs={[
+        { title: 'DNS Log', component: DNSLog },
+        { title: 'Log Settings', component: DNSLogEdit }
+      ]}
+    />
+  )
 }
 
 export default DNSTabView
