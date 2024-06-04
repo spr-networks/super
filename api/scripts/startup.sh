7bf704045dab77469e35f32c7d2f732136093a18
lts-po <po@longterm.io>
2024-06-04 17:36:10 +0200

    [api] remove old cert & generate new for server

--- api/scripts/startup.sh
+++ api/scripts/startup.sh
@@ -7,6 +7,13 @@ echo $MACHINE_ID > /etc/machine-id
 # enable ssl if we have a certificate
 if [ -f "/configs/auth/www-api.key" ] && [ -f "/configs/auth/www-api.crt" ]; then
 	export API_SSL_PORT=443
+
+    # fix update old cert <= 0.3.12
+    if [ -f "/configs/auth/cert/www-api-inter.crt" ]; then
+        echo "+ old cert found, generating new..."
+        rm -f /configs/auth/cert/www-api-inter.*
+        /scripts/generate-certificate.sh
+    fi
 fi
 
 /api
