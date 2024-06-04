f402245e84b67abfb116e664459ca8f471efd266
Alex Rad <alex@supernetworks.org>
2024-06-04 15:36:19 -0700

    [installer] Use correct target script

--- installer/scripts/go-pi.sh
+++ installer/scripts/go-pi.sh
@@ -4,7 +4,7 @@ cp /scripts/pi-cross-install.sh /mnt/fs/tmp/pi-cross-install.sh
 cp /scripts/run-scripts/rc-local /mnt/fs/etc/rc.local
 cp /scripts/spr-environment.sh /mnt/fs/
 cp -R /data/containers /mnt/fs/containers/
-cp /scripts/go-pi-target.sh /mnt/fs/load.sh
+cp /scripts/pi-target-install.sh /mnt/fs/load.sh
 mount --bind /dev/ /mnt/fs/dev/
 # this just downloads packages onto the image. not much else.
 # the rest is done on an aarch64 conatiner with pi-target-install.sh
