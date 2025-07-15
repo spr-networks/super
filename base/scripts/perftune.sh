fba5d987d19e99c235527d63a423f52ee91a489a
Alex Rad <alex@supernetworks.org>
2025-07-15 00:22:11 -0700

    [base] On cm5 dont perftune for now

--- base/scripts/perftune.sh
+++ base/scripts/perftune.sh
@@ -5,6 +5,8 @@
 # The PI board has USB stuck on cpu 0 by default. Since the wifi dongle is stuck there,
 # Move receive/xmit queues off of it
 
+grep -q "Raspberry Pi Compute Module 5 Rev" /proc/cpuinfo && exit
+
 # e -> cpus 1-3, not cpu 0
 
 for x in `echo /sys/class/net/*/queues/*/{r,x}ps_cpus`; do echo e > $x; done
