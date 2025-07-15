b036f4d700da6e36a8d83c180b67a202ecd7e12d
Alex Rad <alex@supernetworks.org>
2025-07-15 00:22:31 -0700

    [watchdog] re-enable watchdog device

--- watchdog/scripts/startup.sh
+++ watchdog/scripts/startup.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+if [ ! -f /dev/watchdog ]; then
+  cat > /configs/watchdog/watchdog.conf << EOF
+watchdog-device = /dev/watchdog
+watchdog-timeout = 15
+EOF
+fi
+
 watchdog -F -c /configs/watchdog/watchdog.conf
+
