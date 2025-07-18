d7a0e64e775f5a8bac203da3ae69add1652933f5
Alex Rad <alex@supernetworks.org>
2025-07-18 16:56:38 -0700

    [build] Fix install script for runsc

--- installer/scripts/install.sh
+++ installer/scripts/install.sh
@@ -106,16 +106,21 @@ EOF
 set -e
 ARCH=$(uname -m)
 URL=https://storage.googleapis.com/gvisor/releases/release/latest/${ARCH}
+pushd /tmp
 wget ${URL}/runsc ${URL}/runsc.sha512 ${URL}/containerd-shim-runsc-v1 ${URL}/containerd-shim-runsc-v1.sha512
 sha512sum -c runsc.sha512
 sha512sum -c containerd-shim-runsc-v1.sha512
 rm -f *.sha512
 chmod a+rx runsc containerd-shim-runsc-v1
-sudo mv runsc containerd-shim-runsc-v1 /usr/local/bin
+mv runsc containerd-shim-runsc-v1 /usr/local/bin
+popd
 
 
 # SPR setup
-mkdir /home/spr
+if [ ! -d /home/spr ]; then 
+  mkdir /home/spr
+fi
+
 cd /home/spr
 cp /tmp/setup.sh .
 cp /tmp/run.sh .
