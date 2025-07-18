8a2ee3a3643523ccc5339ba5ff1e560d42334709
Alex Rad <alex@supernetworks.org>
2025-07-18 11:12:54 -0700

    [build] Pending driver fixes to migrate to 25.04

--- installer/pi-download-image.sh
+++ installer/pi-download-image.sh
@@ -10,7 +10,8 @@ IMG="ubuntu-${VERSION}-preinstalled-server-arm64+raspi.img.xz"
 cd ./data
 
 if [ ! -f $IMG ]; then
-  wget -q "https://cdimage.ubuntu.com/releases/${VERSION}/release/${IMG}"
+  #temp until 25.04 fixes drivers for r8125, mt7915
+  wget -q "https://old-releases.ubuntu.com/releases/oracular/ubuntu-24.10-beta-preinstalled-server-arm64+raspi.img.xz
 fi
 
 xzcat $IMG > spr.clean.img
