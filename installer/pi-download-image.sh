2615b821a31213173f9db7c7f6534ff7ba35a562
Alex Rad <alex@supernetworks.org>
2025-09-11 18:38:25 -0700

    fix typos

--- installer/pi-download-image.sh
+++ installer/pi-download-image.sh
@@ -3,13 +3,13 @@ set -e
 mkdir data
 # 25.04 is not yet ready due to mt7915e, r8125 driver bugs
 VERSION="24.04.3"
-IMG="ubuntu-{$VERSION}-preinstalled-server-arm64+raspi.img.xz"
+IMG="ubuntu-${VERSION}-preinstalled-server-arm64+raspi.img.xz"
 
 cd ./data
 
 if [ ! -f $IMG ]; then
   #temp until 25.04 fixes drivers for r8125, mt7915
-  wget -q "https://cdimage.ubuntu.com/releases/${VERSION}/release/${IMG}
+  wget "https://cdimage.ubuntu.com/releases/${VERSION}/release/${IMG}"
 fi
 
 xzcat $IMG > spr.clean.img
