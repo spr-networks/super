097cca6a7871304425709bf482269b155536e379
Alex Rad <37549748+lts-rad@users.noreply.github.com>
2025-09-11 18:26:18 -0700

    [installer] Use 24.04.3 (#436) #patch

--- installer/pi-download-image.sh
+++ installer/pi-download-image.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 set -e
-
 mkdir data
 # 25.04 is not yet ready due to mt7915e, r8125 driver bugs
-# 24.10 is needed for cm5 support
-VERSION="24.10"
-IMG="ubuntu-${VERSION}-beta-preinstalled-server-arm64+raspi.img.xz"
+VERSION="24.04.3"
+IMG="ubuntu-{$VERSION}-preinstalled-server-arm64+raspi.img.xz"
 
 cd ./data
 
 if [ ! -f $IMG ]; then
   #temp until 25.04 fixes drivers for r8125, mt7915
-  wget -q "https://old-releases.ubuntu.com/releases/oracular/${IMG}"
+  wget -q "https://cdimage.ubuntu.com/releases/${VERSION}/release/${IMG}
 fi
 
 xzcat $IMG > spr.clean.img
