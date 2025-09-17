55bf968ccdc252bcfa1ac8f75163294f03688091
Alex Rad <alex@supernetworks.org>
2025-09-17 15:16:05 -0700

    [installer] For pi update kernel and disable runsc runtime install by default

--- installer/scripts/pi-target-install.sh
+++ installer/scripts/pi-target-install.sh
@@ -121,6 +121,10 @@ pciex4_reset=0
 dtoverlay=pcie-32bit-dma-pi5
 EOF
 
+# make sure to update the kernel / initrd in this qemu environment
+# since apt install may have updated it but not set it
+cp /boot/vmlinuz /boot/firmware/vmlinuz
+cp /boot/initrd.img /boot/firmare/initrd.img
 
 umount /boot/firmware
 rmdir /boot/firmware
@@ -177,15 +181,16 @@ cat > /etc/docker/daemon.json << EOF
 }
 EOF
 
-set -e
-ARCH=$(uname -m)
-URL=https://storage.googleapis.com/gvisor/releases/release/latest/${ARCH}
-wget ${URL}/runsc ${URL}/runsc.sha512 ${URL}/containerd-shim-runsc-v1 ${URL}/containerd-shim-runsc-v1.sha512
-sha512sum -c runsc.sha512
-sha512sum -c containerd-shim-runsc-v1.sha512
-rm -f *.sha512
-chmod a+rx runsc containerd-shim-runsc-v1
-sudo mv runsc containerd-shim-runsc-v1 /usr/local/bin
+
+#set -e
+#ARCH=$(uname -m)
+#URL=https://storage.googleapis.com/gvisor/releases/release/latest/${ARCH}
+#wget ${URL}/runsc ${URL}/runsc.sha512 ${URL}/containerd-shim-runsc-v1 ${URL}/containerd-shim-runsc-v1.sha512
+#sha512sum -c runsc.sha512
+#sha512sum -c containerd-shim-runsc-v1.sha512
+#rm -f *.sha512
+#chmod a+rx runsc containerd-shim-runsc-v1
+#sudo mv runsc containerd-shim-runsc-v1 /usr/local/bin
 
 dockerd  &
 containerd &
