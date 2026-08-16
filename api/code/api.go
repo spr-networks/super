446072f49d06f850164866d284f4611f785503bc
Alex Rad <alex@supernetworks.org>
2026-08-16 14:50:03 -0700

    [api] Add more CSP options

--- api/code/api.go
+++ api/code/api.go
@@ -3198,7 +3198,7 @@ func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func setSecurityHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Security-Policy", "frame-src 'self'")
+		w.Header().Set("Content-Security-Policy", "frame-src 'self'; frame-ancestors 'none'; object-src 'none'; base-uri 'self'; form-action 'self'; manifest-src 'self'")
 		w.Header().Set("X-Frame-Options", "DENY")
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 		next.ServeHTTP(w, r)
